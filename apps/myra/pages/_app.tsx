d466b31caa268653af6999ea51842952cc3a11ec
prajwalpradhan <ctrlx.altf4@gmail.com>
2022-04-14 13:43:23 +0545

    feat: create translate page

--- apps/myra/pages/_app.tsx
+++ apps/myra/pages/_app.tsx
@@ -1,7 +1,9 @@
-import { AppProps } from 'next/app';
+import type { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { ChakraProvider } from '@chakra-ui/react';
 
+import { ChakraProvider } from '@chakra-ui/react';
 // ! TODO move this theme to a separate directory within libs folder
 // 1. Import the extendTheme function
 import { extendTheme } from '@chakra-ui/react';
@@ -44,15 +46,21 @@ const theme = extendTheme({
   },
 });
 
-function CustomApp({ Component, pageProps }: AppProps) {
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+function CustomApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page);
   return (
     <ChakraProvider theme={theme}>
       <Head>
         <title>Welcome to myra!</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <main className="app">{getLayout(<Component {...pageProps} />)}</main>
     </ChakraProvider>
   );
 }
--- apps/myra/pages/translate.tsx
+++ apps/myra/pages/translate.tsx
@@ -1,36 +1,39 @@
 import { Box, Container, Heading } from '@chakra-ui/react';
 import { MainLayout } from '@saccos/myra/ui';
+import { ReactElement } from 'react';
 import { GrClose } from 'react-icons/gr';
 
 // ! TODO separate components
 const Translate = () => {
   return (
-    <MainLayout>
-      <Container
-        maxW="904px"
-        height="fit-content"
-        background="white"
-        mt="5"
-        p="0"
-        pb="55px"
+    <Container
+      maxW="904px"
+      height="fit-content"
+      background="white"
+      mt="5"
+      p="0"
+      pb="55px"
+    >
+      <Box
+        height="60px"
+        display="flex"
+        justifyContent="space-between"
+        alignItems={'center'}
+        style={{
+          borderBottom: '1px solid #E6E6E6',
+        }}
+        px="5"
       >
-        <Box
-          height="60px"
-          display="flex"
-          justifyContent="space-between"
-          alignItems={'center'}
-          style={{
-            borderBottom: '1px solid #E6E6E6',
-          }}
-          px="5"
-        >
-          <Heading size="16px" as="h4">
-            Add New Member
-          </Heading>
-          <GrClose size="14px" color="#91979F" />
-        </Box>
-      </Container>
-    </MainLayout>
+        <Heading size="16px" as="h4">
+          Add New Member
+        </Heading>
+        <GrClose size="14px" color="#91979F" />
+      </Box>
+    </Container>
   );
+};
+
+Translate.getLayout = function getLayout(page: ReactElement) {
+  return <MainLayout>{page}</MainLayout>;
 };
 export default Translate;
