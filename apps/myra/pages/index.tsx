8b61f16f901d86af96db78e57a4c2a7f280c1b9c
prajwalpradhan <ctrlx.altf4@gmail.com>
2022-04-26 11:28:11 +0545

    fix: remove rjsf

--- apps/myra/pages/index.tsx
+++ apps/myra/pages/index.tsx
@@ -11,10 +11,7 @@ import {
   Text,
 } from '@chakra-ui/react';
 import { GrClose } from 'react-icons/gr';
-import { withTheme } from '@rjsf/core';
-import { theme } from '@saccos/myra/util';
 
-const Form = withTheme(theme);
 // NAV
 // import { Flex, Image, InputGroup, InputLeftElement } from '@chakra-ui/react';
 // import { AiOutlineSearch } from 'react-icons/ai';
@@ -25,41 +22,6 @@ const Form = withTheme(theme);
 import { Navbar, TabMenu } from '@saccos/myra/ui';
 // Tab
 
-const schema = {
-  title: 'A registration form',
-  description:
-    'This is the same as the simple form, but it is rendered as a bootstrap grid. Try shrinking the browser window to see it in action.',
-  type: 'object',
-  required: ['firstName', 'lastName'],
-  properties: {
-    firstName: {
-      type: 'string',
-      title: 'First name',
-    },
-    lastName: {
-      type: 'string',
-      title: 'Last name',
-    },
-    age: {
-      type: 'integer',
-      title: 'Age',
-    },
-    bio: {
-      type: 'string',
-      title: 'Bio',
-    },
-    password: {
-      type: 'string',
-      title: 'Password',
-      minLength: 3,
-    },
-    telephone: {
-      type: 'string',
-      title: 'Telephone',
-      minLength: 10,
-    },
-  },
-};
 const forms = [
   {
     label: 'First Name',
@@ -684,7 +646,6 @@ const Index = () => {
           </Box>
         </Box>
       </Container>
-      <Form schema={schema} />
     </>
   );
 };
