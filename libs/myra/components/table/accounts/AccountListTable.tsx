1605dc2f024289a53044d2662c1a4b04b989f318
anup-stha <anup.stha012@gmail.com>
2022-05-27 10:41:43 +0545

    feat: add account form & table

---
+++ libs/myra/components/table/accounts/AccountListTable.tsx
@@ -0,0 +1,70 @@
+import { Column, Table } from '../../../ui/src';
+import { useMemo } from 'react';
+import { IconButton } from '@chakra-ui/react';
+import { BsThreeDots } from 'react-icons/bs';
+import {
+  Gender,
+  useMembersQuery,
+} from '../../../../../apps/myra/generated/graphql';
+
+type MemberData = {
+  id: string;
+  firstName: string;
+  middleName?: string | null;
+  lastName: string;
+  gender: Gender;
+  title?: string | null;
+  dateOfBirth?: string | null;
+};
+
+export const AccountListTable = () => {
+  const columns: Column<MemberData>[] = useMemo(
+    () => [
+      {
+        Header: 'Account Code',
+        accessor: 'id',
+        maxWidth: 4,
+      },
+      {
+        Header: 'Name',
+        accessor: 'firstName',
+        width: '80%',
+      },
+      {
+        Header: 'Account Type',
+        accessor: 'title',
+        width: '40%',
+      },
+
+      {
+        Header: 'Parent Group',
+        accessor: 'gender',
+        width: '40%',
+      },
+
+      {
+        accessor: 'actions',
+        Cell: () => (
+          <IconButton
+            variant="ghost"
+            aria-label="Search database"
+            icon={<BsThreeDots />}
+          />
+        ),
+      },
+    ],
+    []
+  );
+  const { data, isLoading } = useMembersQuery();
+
+  const rowData = useMemo(() => data && data?.members?.list, [data]);
+
+  return (
+    <Table
+      isLoading={isLoading}
+      data={rowData?.slice(0, 10) ?? []}
+      columns={columns}
+      sort={true}
+    />
+  );
+};
