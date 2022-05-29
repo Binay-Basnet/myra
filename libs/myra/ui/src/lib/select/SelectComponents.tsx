a9df0a74d3b8cf52939ccf2d377fc4fb93db0508
anup-stha <anup.stha012@gmail.com>
2022-05-29 19:30:55 +0545

    feat: add multi select

---
+++ libs/myra/ui/src/lib/select/SelectComponents.tsx
@@ -0,0 +1,33 @@
+import {
+  chakraComponents,
+  GroupBase,
+  SelectComponentsConfig,
+} from 'chakra-react-select';
+import { Checkbox, Icon } from '@chakra-ui/react';
+import { IoCheckmarkSharp, IoChevronDownSharp } from 'react-icons/io5';
+
+export const components: SelectComponentsConfig<
+  unknown,
+  boolean,
+  GroupBase<unknown>
+> = {
+  DropdownIndicator: (props) => (
+    <chakraComponents.DropdownIndicator {...props}>
+      <Icon as={IoChevronDownSharp} w="20px" h="20px" cursor="pointer" />
+    </chakraComponents.DropdownIndicator>
+  ),
+  Option: ({ children, ...props }) => {
+    console.log(props.isMulti, props.isSelected);
+
+    return (
+      <chakraComponents.Option {...props}>
+        {children}
+        {props.isMulti ? (
+          <Checkbox isChecked={props.isSelected} />
+        ) : props.isSelected ? (
+          <Icon as={IoCheckmarkSharp} w="20px" h="20px" cursor="pointer" />
+        ) : null}
+      </chakraComponents.Option>
+    );
+  },
+};
