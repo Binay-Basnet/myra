1050777e437a34150b354f4fe421b75b5f2bc89b
ctrlx-altf4 <ctrlx.altf4@gmail.com>
2022-06-03 10:09:03 +0545

    feat: implement tree view of COa

---
+++ libs/myra/components/src/lib/chartOfAccounts/Tree/TreeHeader.tsx
@@ -0,0 +1,38 @@
+import { BsFillCaretRightFill } from 'react-icons/bs';
+import { HStack, Icon, Text } from '@chakra-ui/react';
+import { Button } from '@saccos/myra/ui';
+
+import { useAccordion } from './Accordion';
+
+interface ITreeHeaderProps {
+  code: string;
+  title: string;
+  isExtensible?: boolean;
+}
+
+function TreeHeader(props: ITreeHeaderProps) {
+  const { isOpen } = useAccordion();
+  const { code, title, isExtensible } = props;
+  return (
+    <HStack ml="-7px" alignItems="center">
+      <Icon
+        pl={1}
+        as={BsFillCaretRightFill}
+        color="gray.500"
+        transform={isOpen ? 'rotate(90deg)' : 'rotate(0deg)'}
+        transition="0.4s all ease"
+      />
+      <HStack>
+        <Text fontWeight="bold" fontSize="14px">
+          {code}
+        </Text>
+        <Text fontWeight="400" fontSize="14px" color="gray.800">
+          {title}
+        </Text>
+        {/* {isExtensible && <Button variant="link">Add Group</Button>} */}
+      </HStack>
+    </HStack>
+  );
+}
+
+export default TreeHeader;
