410f78419e3af0d75edc496eec1d68a4ee50c892
Suyash Shakya <suyash.shakya@dragonlaw.com.hk>
2022-06-07 11:47:46 +0545

    fix: changes

--- libs/myra/util/src/index.ts
+++ libs/myra/util/src/index.ts
@@ -2,5 +2,6 @@ export * from './hooks/useTableHook';
 export * from './hooks/useTranslation';
 export * from './lib/myra-util';
 export * from './lib/theme';
+export * from './redux/store';
 export * from './utilFunctions/translation';
 export * from './validationSchema/memberForm';
