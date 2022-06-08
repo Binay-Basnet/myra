8fc2849da778539df8bc4bd93f3eef931772edc4
anup-stha <anup.stha012@gmail.com>
2022-06-08 19:29:57 +0545

    fix: radio in general settings. issue 55

--- libs/myra/components/src/lib/setings/general/organizations/RadioOrganization.tsx
+++ libs/myra/components/src/lib/setings/general/organizations/RadioOrganization.tsx
@@ -1,4 +1,4 @@
-import { Grid, Radio, RadioGroup, RadioGroupProps } from '@chakra-ui/react';
+import { Grid, Radio, RadioGroup } from '@chakra-ui/react';
 // import{ Control} from 'react-hook-form';
 
 // interface Props extends RadioGroupProps{
@@ -8,16 +8,16 @@ export const RadioOrganization = () => {
   return (
     <RadioGroup fontWeight={'400'}>
       {/* <Box display="flex" justifyContent={'flex-start'}> */}
-      <Grid templateColumns={'repeat(5, 1fr)'} gap={'s16'}>
-        <Radio value="1"> Individual</Radio>
-        <Radio value="2"> Institutional</Radio>
+      <Grid templateRows={'repeat(2, 1fr)'} gap={'s8'}>
+        <Radio size="md" value="1">
+          Cooperative
+        </Radio>
+        <Radio value="2" size="md">
+          Cooperative Union
+        </Radio>
       </Grid>
       {/* </Box> */}
       {/* <Box display="flex" justifyContent={'flex-start'}> */}
-      <Grid templateColumns={'repeat(5, 1fr)'} gap={'s16'}>
-        <Radio value="3"> CoOperative</Radio>
-        <Radio value="4"> CoOperative Union</Radio>
-      </Grid>
       {/* </Box> */}
     </RadioGroup>
   );
