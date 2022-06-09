ca2da9f029739fd9f42954dc0e3f046ffa3431a1
Suyash Shakya <suyash.shakya@dragonlaw.com.hk>
2022-06-09 20:11:38 +0545

    fix: changes

--- libs/myra/components/src/lib/layout/MemberLayout.tsx
+++ libs/myra/components/src/lib/layout/MemberLayout.tsx
@@ -42,10 +42,11 @@ interface memberTypeButtonProps {
   title: string;
   subtitle: string;
   onClick?: MouseEventHandler<HTMLDivElement>;
+  disabled: boolean;
 }
 
 const MemberTypeButton = (props: memberTypeButtonProps) => {
-  const { icon, title, subtitle, onClick } = props;
+  const { icon, title, subtitle, onClick, disabled } = props;
   return (
     <Box
       as="button"
@@ -57,12 +58,15 @@ const MemberTypeButton = (props: memberTypeButtonProps) => {
       borderRadius="6px"
       onClick={onClick}
       boxShadow={'none'}
-      color="#4b4f56"
-      _hover={{ bg: '#ebedf0', color: 'primary.500' }}
+      color={disabled ? 'gray.500' : '#4b4f56'}
+      _hover={{
+        bg: disabled ? '' : '#ebedf0',
+        color: disabled ? '' : 'primary.500',
+      }}
       _active={{
-        bg: '#dddfe2',
-        transform: 'scale(0.98)',
-        borderColor: 'primary.500',
+        bg: disabled ? '' : '#dddfe2',
+        transform: disabled ? '' : 'scale(0.98)',
+        borderColor: disabled ? '' : 'primary.500',
       }}
     >
       <Icon size="xl" as={icon} />
@@ -163,13 +167,14 @@ export const MemberPagesLayout = ({ children }: IMemberPageLayout) => {
                     icon={memberTypesArray[item.type]?.icon}
                     title={memberTypesArray[item.type]?.title}
                     subtitle={memberTypesArray[item.type]?.subtitle}
-                    // disabled={item?.type !== 'INDIVIDUAL'}
+                    disabled={item?.type !== 'INDIVIDUAL'}
                     onClick={() => {
-                      newId
-                        .mutateAsync({})
-                        .then((res) =>
-                          router.push(`/members/add/${res?.newId}`)
-                        );
+                      item?.type === 'INDIVIDUAL' &&
+                        newId
+                          .mutateAsync({})
+                          .then((res) =>
+                            router.push(`/members/add/${res?.newId}`)
+                          );
                     }}
                   />
                 </GridItem>
