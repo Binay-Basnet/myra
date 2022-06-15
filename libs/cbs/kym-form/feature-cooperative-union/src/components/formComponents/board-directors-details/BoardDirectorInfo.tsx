f33a26a476a54e65270eee07620f9128e46d5a01
sameergautam01 <sameer.gautam9860@gmail.com>
2022-06-15 15:43:21 +0545

    fix: director form function

--- libs/cbs/kym-form/feature-cooperative-union/src/components/formComponents/board-directors-details/BoardDirectorInfo.tsx
+++ libs/cbs/kym-form/feature-cooperative-union/src/components/formComponents/board-directors-details/BoardDirectorInfo.tsx
@@ -9,10 +9,12 @@ import {
 import { FaMap } from 'react-icons/fa';
 import { CloseIcon } from '@chakra-ui/icons';
 import {
+  AccordianContainer,
   DynamicBoxContainer,
   DynamicBoxGroupContainer,
   GroupContainer,
   InputGroupContainer,
+  SectionContainer,
 } from '@coop/cbs/kym-form/ui-containers';
 import {
   FormFileInput,
@@ -96,6 +98,8 @@ const AddDirector = ({ watch, index, control, removeDirector }) => {
           display="flex"
           justifyContent="space-between"
           alignItems="center"
+          cursor={'pointer'}
+          onClick={() => setIsOpen(!isOpen)}
         >
           <Text fontSize="r1">{`Director ${index + 1}`}</Text>
           <Box>
@@ -105,7 +109,6 @@ const AddDirector = ({ watch, index, control, removeDirector }) => {
                 variant={'ghost'}
                 aria-label="close"
                 icon={<AiOutlineCaretDown />}
-                onClick={() => setIsOpen(false)}
               />
             ) : (
               <IconButton
@@ -113,226 +116,237 @@ const AddDirector = ({ watch, index, control, removeDirector }) => {
                 variant={'ghost'}
                 aria-label="close"
                 icon={<AiOutlineCaretRight />}
-                onClick={() => setIsOpen(true)}
               />
             )}
           </Box>
         </Box>
-        <IconButton
-          size="sm"
-          variant={'ghost'}
-          aria-label="close"
-          icon={<CloseIcon />}
-          onClick={removeDirector}
-        />
+        {!isOpen && (
+          <IconButton
+            size="sm"
+            variant={'ghost'}
+            aria-label="close"
+            icon={<CloseIcon />}
+            ml="s16"
+            onClick={removeDirector}
+          />
+        )}
       </Box>
 
-      <DynamicBoxGroupContainer>
-        <Collapse in={isOpen} style={{ marginTop: '0px' }}>
-          <InputGroupContainer>
-            <FormInput
-              type="text"
-              name={`boardOfDirectorsDetails.${index}.fullName`}
-              label="Full Name"
-              placeholder="Enter Full Name"
-            />
-            <FormInput
-              type="text"
-              name={`boardOfDirectorsDetails.${index}.designation`}
-              label="Designation"
-              placeholder="Enter Designation"
-            />
-          </InputGroupContainer>
-          <Text fontSize="r1" fontWeight="SemiBold">
-            Permanent Address
-          </Text>
-          <Box
-            id="Permanent Address"
-            gap="s32"
-            display={'flex'}
-            flexDirection="column"
-          >
+      {/* <DynamicBoxGroupContainer> */}
+      <Collapse in={isOpen} style={{ marginTop: '0px' }}>
+        <DynamicBoxGroupContainer>
+          <SectionContainer>
+            <AccordianContainer>
+              <InputGroupContainer>
+                <FormInput
+                  type="text"
+                  name={`boardOfDirectorsDetails.${index}.fullName`}
+                  label="Full Name"
+                  placeholder="Enter Full Name"
+                />
+                <FormInput
+                  type="text"
+                  name={`boardOfDirectorsDetails.${index}.designation`}
+                  label="Designation"
+                  placeholder="Enter Designation"
+                />
+              </InputGroupContainer>
+            </AccordianContainer>
+
+            <AccordianContainer>
+              <Text fontSize="r1" fontWeight="SemiBold">
+                Permanent Address
+              </Text>
+              {/* <Box
+              id="Permanent Address"
+              gap="s32"
+              display={'flex'}
+              flexDirection="column"
+            > */}
+              <InputGroupContainer>
+                <FormSelect
+                  name={`boardOfDirectorsDetails.${index}.permanentStateId`}
+                  label="State"
+                  placeholder="Select State"
+                  options={province}
+                />
+                <FormSelect
+                  name={`boardOfDirectorsDetails.${index}.permanentDistrictId`}
+                  label="District"
+                  placeholder="Select District"
+                  options={districtList.map((d) => ({
+                    label: d.name,
+                    value: d.id,
+                  }))}
+                />
+                <FormSelect
+                  name={`boardOfDirectorsDetails.${index}.permanentVdcOrMunicId`}
+                  label="VDC / Municipality"
+                  placeholder="Select VDC / Municipality"
+                  options={localityList.map((d) => ({
+                    label: d.name,
+                    value: d.id,
+                  }))}
+                />
+                <FormInput
+                  type="number"
+                  name={`boardOfDirectorsDetails.${index}.permanentWardId`}
+                  label="Ward No"
+                  placeholder="Enter Ward No"
+                />
+                <FormInput
+                  type="text"
+                  name={`boardOfDirectorsDetails.${index}.permanentLocality`}
+                  label="Locality"
+                  placeholder="Enter Locality"
+                />
+              </InputGroupContainer>
+
+              <Button
+                alignSelf="start"
+                leftIcon={<Icon size="md" as={FaMap} />}
+              >
+                Pin on Map
+              </Button>
+              {/* </Box> */}
+            </AccordianContainer>
+
+            <Box
+              id="Temporary Address"
+              gap="s32"
+              display={'flex'}
+              flexDirection="column"
+              scrollMarginTop={'200px'}
+            >
+              <Text fontSize="r1" fontWeight="SemiBold">
+                Temporary Address
+              </Text>
+
+              <FormSwitch
+                control={control}
+                name="isPermanentAndTemporaryAddressSame"
+                label="Temporary Address same as permanent"
+              />
+
+              <InputGroupContainer>
+                <FormSelect
+                  name={`boardOfDirectorsDetails.${index}.temporaryStateId`}
+                  label="State"
+                  placeholder="Select State"
+                  options={province}
+                />
+                <FormSelect
+                  name={`boardOfDirectorsDetails.${index}.temporaryDistrictId`}
+                  label="District"
+                  placeholder="Select District"
+                  options={districtTempList.map((d) => ({
+                    label: d.name,
+                    value: d.id,
+                  }))}
+                />
+                <FormSelect
+                  name={`boardOfDirectorsDetails.${index}.temporaryVdcOrMunicId`}
+                  label="VDC / Muncipality"
+                  placeholder="Select VDC / Muncipality"
+                  options={localityTempList.map((d) => ({
+                    label: d.name,
+                    value: d.id,
+                  }))}
+                />
+                <FormInput
+                  type="number"
+                  name={`boardOfDirectorsDetails.${index}.temporaryWardId`}
+                  label="Ward No"
+                  placeholder="Enter Ward No"
+                />
+                <FormInput
+                  type="text"
+                  name={`boardOfDirectorsDetails.${index}.temporaryLocality`}
+                  label="Locality"
+                  placeholder="Enter Locality"
+                />
+              </InputGroupContainer>
+              <Button
+                mt="-16px"
+                alignSelf="start"
+                leftIcon={<Icon size="md" as={FaMap} />}
+              >
+                Pin on Map
+              </Button>
+            </Box>
             <InputGroupContainer>
-              <FormSelect
-                name={`boardOfDirectorsDetails.${index}.permanentStateId`}
-                label="State"
-                placeholder="Select State"
-                options={province}
-              />
-              <FormSelect
-                name={`boardOfDirectorsDetails.${index}.permanentDistrictId`}
-                label="District"
-                placeholder="Select District"
-                options={districtList.map((d) => ({
-                  label: d.name,
-                  value: d.id,
-                }))}
-              />
-              <FormSelect
-                name={`boardOfDirectorsDetails.${index}.permanentVdcOrMunicId`}
-                label="VDC / Municipality"
-                placeholder="Select VDC / Municipality"
-                options={localityList.map((d) => ({
-                  label: d.name,
-                  value: d.id,
-                }))}
-              />
               <FormInput
-                type="number"
-                name={`boardOfDirectorsDetails.${index}.permanentWardId`}
-                label="Ward No"
-                placeholder="Enter Ward No"
+                type="date"
+                name={`boardOfDirectorsDetails.${index}.dateOfMembership`}
+                label="Date of membership"
+                placeholder="DD-MM-YYYY"
               />
               <FormInput
                 type="text"
-                name={`boardOfDirectorsDetails.${index}.permanentLocality`}
-                label="Locality"
-                placeholder="Enter Locality"
+                name={`boardOfDirectorsDetails.${index}.highestQualification`}
+                label="Highest Qualification"
+                placeholder="Enter higest qualification"
+              />
+              <FormInput
+                type="number"
+                name={`boardOfDirectorsDetails.${index}.contactNumber`}
+                label="Mobile No"
+                placeholder="Enter Mobile No"
+              />
+              <FormInput
+                type="text"
+                name={`boardOfDirectorsDetails.${index}.email`}
+                label="Email"
+                placeholder="Enter Email"
+              />
+              <FormInput
+                type="string"
+                name={`boardOfDirectorsDetails.${index}.citizenshipOrPassportOrLisenceNo`}
+                label="Citizenship/Passport/Driving License No."
+                placeholder="Enter No"
               />
             </InputGroupContainer>
-
-            <Button
-              alignSelf="start"
-              mt="-16px"
-              leftIcon={<Icon size="md" as={FaMap} />}
-            >
-              Pin on Map
-            </Button>
-          </Box>
-          <Box
-            id="Temporary Address"
-            gap="s32"
-            display={'flex'}
-            flexDirection="column"
-            scrollMarginTop={'200px'}
-          >
             <Text fontSize="r1" fontWeight="SemiBold">
-              Temporary Address
+              Training related to Co-operatives
             </Text>
-
-            <FormSwitch
-              control={control}
-              name="isPermanentAndTemporaryAddressSame"
-              label="Temporary Address same as permanent"
-            />
-
             <InputGroupContainer>
-              <FormSelect
-                name={`boardOfDirectorsDetails.${index}.temporaryStateId`}
-                label="State"
-                placeholder="Select State"
-                options={province}
+              <FormInput
+                type="text"
+                name={`boardOfDirectorsDetails.${index}.subjectOfTraining`}
+                label="Subject of Training"
+                placeholder="Enter Subject of Training"
               />
-              <FormSelect
-                name={`boardOfDirectorsDetails.${index}.temporaryDistrictId`}
-                label="District"
-                placeholder="Select District"
-                options={districtTempList.map((d) => ({
-                  label: d.name,
-                  value: d.id,
-                }))}
-              />
-              <FormSelect
-                name={`boardOfDirectorsDetails.${index}.temporaryVdcOrMunicId`}
-                label="VDC / Muncipality"
-                placeholder="Select VDC / Muncipality"
-                options={localityTempList.map((d) => ({
-                  label: d.name,
-                  value: d.id,
-                }))}
+              <FormInput
+                type="date"
+                name={`boardOfDirectorsDetails.${index}.dateOfTraining`}
+                label="Date of training"
+                placeholder="Enter date of Training"
               />
               <FormInput
                 type="number"
-                name={`boardOfDirectorsDetails.${index}.temporaryWardId`}
-                label="Ward No"
-                placeholder="Enter Ward No"
-              />
-              <FormInput
-                type="text"
-                name={`boardOfDirectorsDetails.${index}.temporaryLocality`}
-                label="Locality"
-                placeholder="Enter Locality"
+                name={`boardOfDirectorsDetails.${index}.trainingOrganization`}
+                label="Training Organization"
+                placeholder="Enter Training Organization"
               />
             </InputGroupContainer>
-            <Button
-              mt="-16px"
-              alignSelf="start"
-              leftIcon={<Icon size="md" as={FaMap} />}
-            >
-              Pin on Map
-            </Button>
-          </Box>
-          <InputGroupContainer>
-            <FormInput
-              type="date"
-              name={`boardOfDirectorsDetails.${index}.dateOfMembership`}
-              label="Date of membership"
-              placeholder="DD-MM-YYYY"
-            />
-            <FormInput
-              type="text"
-              name={`boardOfDirectorsDetails.${index}.highestQualification`}
-              label="Highest Qualification"
-              placeholder="Enter higest qualification"
-            />
-            <FormInput
-              type="number"
-              name={`boardOfDirectorsDetails.${index}.contactNumber`}
-              label="Mobile No"
-              placeholder="Enter Mobile No"
-            />
-            <FormInput
-              type="text"
-              name={`boardOfDirectorsDetails.${index}.email`}
-              label="Email"
-              placeholder="Enter Email"
-            />
-            <FormInput
-              type="string"
-              name={`boardOfDirectorsDetails.${index}.citizenshipOrPassportOrLisenceNo`}
-              label="Citizenship/Passport/Driving License No."
-              placeholder="Enter No"
-            />
-          </InputGroupContainer>
-          <Text fontSize="r1" fontWeight="SemiBold">
-            Training related to Co-operatives
-          </Text>
-          <InputGroupContainer>
-            <FormInput
-              type="text"
-              name={`boardOfDirectorsDetails.${index}.subjectOfTraining`}
-              label="Subject of Training"
-              placeholder="Enter Subject of Training"
-            />
-            <FormInput
-              type="date"
-              name={`boardOfDirectorsDetails.${index}.dateOfTraining`}
-              label="Date of training"
-              placeholder="Enter date of Training"
-            />
-            <FormInput
-              type="number"
-              name={`boardOfDirectorsDetails.${index}.trainingOrganization`}
-              label="Training Organization"
-              placeholder="Enter Training Organization"
-            />
-          </InputGroupContainer>
-          <Grid templateColumns="repeat(2, 1fr)" rowGap="s32" columnGap="s20">
-            <FormFileInput
-              size="md"
-              label="Photograph"
-              // control={control}
-              name={`boardOfDirectorsDetails.${index}.photograph`}
-            />
-            <FormFileInput
-              size="md"
-              label="Photograph of identity proof document"
-              // control={control}
-              name={`boardOfDirectorsDetails.${index}.identityDocumentPhoto`}
-            />
-          </Grid>
-        </Collapse>
-      </DynamicBoxGroupContainer>
+            <Grid templateColumns="repeat(2, 1fr)" rowGap="s32" columnGap="s20">
+              <FormFileInput
+                size="md"
+                label="Photograph"
+                // control={control}
+                name={`boardOfDirectorsDetails.${index}.photograph`}
+              />
+              <FormFileInput
+                size="md"
+                label="Photograph of identity proof document"
+                // control={control}
+                name={`boardOfDirectorsDetails.${index}.identityDocumentPhoto`}
+              />
+            </Grid>
+          </SectionContainer>
+        </DynamicBoxGroupContainer>
+      </Collapse>
+      {/* </DynamicBoxGroupContainer> */}
     </>
   );
 };
@@ -350,7 +364,7 @@ export const BoardDirectorInfo = ({ watch, control }) => {
       </Text>
       {directorFields.map((item, index) => {
         return (
-          <Box key={item.id}>
+          <Box key={item.id} display="flex" flexDirection={'column'} gap="s16">
             <AddDirector
               watch={watch}
               index={index}
