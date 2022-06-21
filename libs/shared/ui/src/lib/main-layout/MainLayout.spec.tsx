639aeb4bae46419faa1ae4830b3cc3f596bf1a43
anup-stha <anup.stha012@gmail.com>
2022-06-21 22:29:00 +0545

    fix: test

--- libs/shared/ui/src/lib/main-layout/MainLayout.spec.tsx
+++ libs/shared/ui/src/lib/main-layout/MainLayout.spec.tsx
@@ -1,10 +1,16 @@
+import { RouterContext } from 'next/dist/shared/lib/router-context';
 import { render } from '@testing-library/react';
 
 import MainLayout from './MainLayout';
+import { createMockRouter } from '../table-search/TableSearch.spec';
 
 describe('MainLayout', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<MainLayout />);
+    const { baseElement } = render(
+      <RouterContext.Provider value={createMockRouter({ query: { id: '22' } })}>
+        <MainLayout> Hello Test </MainLayout>{' '}
+      </RouterContext.Provider>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
