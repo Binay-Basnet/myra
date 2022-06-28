f01b57d5a2f8ea88489c6c0caa03dd9897ac9f48
anup-stha <anup.stha012@gmail.com>
2022-06-28 19:04:45 +0545

    Revert "fix: test"
    This reverts commit a10946a0

---
+++ libs/shared/map/src/lib/LeafletControlGeocoder.tsx
@@ -0,0 +1,39 @@
+import React, { SetStateAction, useEffect } from 'react';
+import { useMap } from 'react-leaflet';
+import Geocoder from 'leaflet-control-geocoder';
+
+import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
+// import 'leaflet-control-geocoder/dist/Control.Geocoder.js';
+
+export default function LeafletControlGeocoder(props: {
+  setPosition: React.Dispatch<
+    SetStateAction<{ latitude: number; longitude: number }>
+  >;
+}) {
+  const { setPosition } = props;
+  const map = useMap();
+
+  useEffect(() => {
+    const geocoder = new Geocoder({
+      query: '',
+      placeholder: 'Search here...',
+      defaultMarkGeocode: false,
+      suggestMinLength: 2,
+    });
+
+    geocoder
+      .on('markgeocode', function (e) {
+        setPosition({
+          latitude: e?.geocode?.center?.lat,
+          longitude: e?.geocode?.center?.lng,
+        });
+        map.setView({
+          lat: e?.geocode?.center?.lat,
+          lng: e?.geocode?.center?.lng,
+        });
+      })
+      .addTo(map);
+  }, []);
+
+  return null;
+}
