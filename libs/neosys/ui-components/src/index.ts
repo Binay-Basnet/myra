ffd600d121e592f8ccb256fcdc378b16c5db41b6
Ajay Prajapati <ajayprazz@gmail.com>
2022-07-06 10:56:09 +0545

    feat: update client details

--- libs/neosys/ui-components/src/index.ts
+++ libs/neosys/ui-components/src/index.ts
@@ -1,3 +1,5 @@
+export * from './lib/NeosysDocumentCard';
+export * from './lib/NeosysDocumentCard';
 export * from './lib/NeosysPageHeaderTab';
 export * from './lib/NeosysTableListPageHeader';
 export * from './lib/NeosysUiComponents';
