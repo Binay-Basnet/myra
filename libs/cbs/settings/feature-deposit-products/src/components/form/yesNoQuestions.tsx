43ab72e0dc00af0477ad6728973b092f8ff4ea33
sameergautam01 <sameer.gautam9860@gmail.com>
2022-07-08 06:46:44 +0545

    fix: deposit prroduct fixes

--- libs/cbs/settings/feature-deposit-products/src/components/form/yesNoQuestions.tsx
+++ libs/cbs/settings/feature-deposit-products/src/components/form/yesNoQuestions.tsx
@@ -6,7 +6,7 @@ import {
   InputGroupContainer,
 } from '@coop/cbs/kym-form/ui-containers';
 import { FormInput, FormSwitchTab } from '@coop/shared/form';
-import { Box, Text } from '@coop/shared/ui';
+import { Box, GridItem, Text } from '@coop/shared/ui';
 
 import { DividerContainer, SubHeadingText } from '../formui';
 
@@ -16,6 +16,8 @@ const yesNo = [
 ];
 export const Questions = ({ watch }: any) => {
   const depositNature = watch('nameOfDepositProduct');
+  const allowLoan = watch('allowLoan');
+  const withdrawRestricted = watch('withdrawRestricted');
   return (
     <DividerContainer>
       <Box display="flex" flexDirection={'row'} justifyContent="space-between">
@@ -48,13 +50,39 @@ export const Questions = ({ watch }: any) => {
       )}
       {(depositNature === 'recurringSaving' ||
         depositNature === 'termSaving') && (
-        <Box
-          display="flex"
-          flexDirection={'row'}
-          justifyContent="space-between"
-        >
-          <SubHeadingText>Allow Loan</SubHeadingText>
-          <FormSwitchTab name={'allowLoan'} options={yesNo} />
+        <Box display={'flex'} flexDirection="column" gap="s16">
+          <Box
+            display="flex"
+            flexDirection={'row'}
+            justifyContent="space-between"
+          >
+            <SubHeadingText>Allow Loan</SubHeadingText>
+            <FormSwitchTab name={'allowLoan'} options={yesNo} />
+          </Box>
+
+          {(depositNature === 'recurringSaving' ||
+            depositNature === 'termSaving') &&
+            allowLoan &&
+            allowLoan === 'yes' && (
+              <Box
+                display={'flex'}
+                justifyContent="space-between"
+                p="s16"
+                border="1px solid"
+                borderColor={'border.layout'}
+                borderRadius="6px"
+              >
+                <InputGroupContainer>
+                  <FormInput
+                    type="number"
+                    name="percentageOfDeposit"
+                    label="Percentage of Deposit"
+                    placeholder="0.00"
+                    rightElement="%"
+                  />
+                </InputGroupContainer>
+              </Box>
+            )}
         </Box>
       )}
       {depositNature !== 'mandatory' && (
@@ -70,6 +98,38 @@ export const Questions = ({ watch }: any) => {
       <Box display="flex" flexDirection={'row'} justifyContent="space-between">
         <SubHeadingText>Staff Product</SubHeadingText>
         <FormSwitchTab name={'staffProduct'} options={yesNo} />
+      </Box>
+      <Box display={'flex'} flexDirection="column" gap="s16">
+        <Box
+          display="flex"
+          flexDirection={'row'}
+          justifyContent="space-between"
+        >
+          <SubHeadingText>Widthdraw Restricted</SubHeadingText>
+          <FormSwitchTab name={'withdrawRestricted'} options={yesNo} />
+        </Box>
+        {withdrawRestricted && withdrawRestricted === 'yes' && (
+          <Box
+            border="1px solid"
+            borderColor="border.layout"
+            borderRadius="6px"
+            p="s16"
+            minH="156px"
+          >
+            <Box>
+              <FormInput
+                type="text"
+                name="specifyRestrictedwithdraw"
+                label="Specify,"
+                minH={'100px'}
+              />{' '}
+            </Box>
+          </Box>
+        )}
+      </Box>
+      <Box display="flex" flexDirection={'row'} justifyContent="space-between">
+        <SubHeadingText>Wealth Building Product</SubHeadingText>
+        <FormSwitchTab name={'wealthBuildingProject'} options={yesNo} />
       </Box>
     </DividerContainer>
   );
