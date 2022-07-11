040012a0d5337439ce5bbe375127703a71bdb0e9
Ajay Prajapati <ajayprazz@gmail.com>
2022-07-11 10:50:01 +0545

    feat: add accounting purchase and ui-components library

---
+++ libs/accounting/feature-purchase/src/lib/AccountingFeaturePurchase.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable-next-line */
+export interface AccountingFeaturePurchaseProps {}
+
+export function AccountingFeaturePurchase(
+  props: AccountingFeaturePurchaseProps
+) {
+  return (
+    <div>
+      <h1>Welcome to AccountingFeaturePurchase!</h1>
+    </div>
+  );
+}
+
+export default AccountingFeaturePurchase;
