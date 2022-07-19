f15d188a8fe21f9be66778d2721336dee6884bd4
Anup Shrestha <anup.stha012@gmail.com>
2022-07-19 21:00:19 +0545

    feat: folder structure for coop

---
+++ apps/ebanking/pages/coop/downloads/directives.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from 'react';
+
+import { EbankingMainLayout } from '@coop/ebanking/ui-layout';
+
+import Temp from '../../temp';
+
+const COOPDirectivesDownloadsPage = () => {
+  return <Temp />;
+};
+
+COOPDirectivesDownloadsPage.getLayout = function (page: ReactElement) {
+  return <EbankingMainLayout>{page}</EbankingMainLayout>;
+};
+
+export default COOPDirectivesDownloadsPage;
