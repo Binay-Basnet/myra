51d4def703565d2d03801917a06a553518a2bdb6
Anup Shrestha <anup.stha012@gmail.com>
2022-07-19 18:40:32 +0545

    feat: add accounts folder structure

---
+++ apps/ebanking/pages/accounts/[id].tsx
@@ -0,0 +1,7 @@
+import Home from 'apps/ebanking/pages/home';
+
+const AccountDetailPage = () => {
+  return <Home />;
+};
+
+export default AccountDetailPage;
---
+++ apps/ebanking/pages/accounts/index.tsx
@@ -0,0 +1,7 @@
+import Home from 'apps/ebanking/pages/home';
+
+const AccountsListPage = () => {
+  return <Home />;
+};
+
+export default AccountsListPage;
