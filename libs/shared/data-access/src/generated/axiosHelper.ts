58ac83ead0ece09d8234df2b639c639a87fc514f
suyash shakya <suyashshakya.h0p3@gmail.com>
2022-07-25 17:52:06 +0545

    fix: changes

--- libs/shared/data-access/src/generated/axiosHelper.ts
+++ libs/shared/data-access/src/generated/axiosHelper.ts
@@ -1,29 +1,5 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
-import { useAuth } from '@coop/shared/data-access';
-
-const refreshToken = (url: string) => {
-  const refreshToken = localStorage.getItem('_refr');
-  return axios
-    .post(url, {
-      query: `mutation{
-  auth{
-    token(refreshToken:"${refreshToken}"){
-      refresh
-      access
-    }
-  }
-}`,
-    })
-    .then((res) => {
-      return res.data?.access;
-    })
-    .catch((err) => {
-      alert('redirect to login');
-    });
-};
-
-// import { useAuth } from '@coop/shared/utils';
 export const useAxios = <TData, TVariables>(
   query: string
 ): ((
@@ -35,58 +11,113 @@ export const useAxios = <TData, TVariables>(
   const url = process.env['NX_SCHEMA_PATH'] ?? '';
   // return axios.post<TData>(url,data:{query,variables}).then((res) => res.data);
 
-  const auth = useAuth();
-
-  const accessToken = auth?.auth?.accessToken;
-  console.log('access', accessToken); // get this token from redux
-
   return async (variables?: TVariables, config?: AxiosRequestConfig<TData>) => {
-    const headers = {
-      Authoirzation: `Bearer ${accessToken}`,
-    };
-    if (config) {
-      if (config.headers) {
-        config.headers = { ...config.headers, ...headers };
-      } else {
-        config.headers = { ...headers };
-      }
-    } else {
-      config = {
-        headers,
-      };
-    }
     return axios
       .post<{ data: TData }>(url, { query, variables }, config)
       .then((res: AxiosResponse<{ data: TData; errors?: any[] }>) => {
-        // IF access token is invalid
-        // if()
-        // refreshToken(url).then((res) => {
-        //   console.log('token', res.token);
-        //   const headers = {
-        //     Authoirzation: `Bearer ${res.token}`,
-        //   };
-        //   if (config) {
-        //     if (config.headers) {
-        //       config.headers = { ...config.headers, ...headers };
-        //     } else {
-        //       config.headers = { ...headers };
-        //     }
-        //   } else {
-        //     config = {
-        //       headers,
-        //     };
-        //   }
-
-        //   return axios
-        //     .post<{ data: TData }>(url, { query, variables }, config)
-        //     .then((res) => {
-        //       return res.data.data;
-        //     });
-        // });
+        // IF ERROR, THROW AN ERROR !!
+        // if (res.data.errors) {
+        //   res.data.errors.map((error) => {
+        //     const updatedError = new Error('Unknown Server Error');
+        //     updatedError.message =
+        //       error?.message + '\n Path: ' + error?.path.toString();
+        //     updatedError.stack = error?.path;
+        //     throw updatedError;
+        //   });
+        // }
         return res.data.data;
       });
   };
 };
+// import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+// import { useAuth } from '@coop/shared/data-access';
+
+// const refreshToken = (url: string) => {
+//   const refreshToken = localStorage.getItem('_refr');
+//   return axios
+//     .post(url, {
+//       query: `mutation{
+//   auth{
+//     token(refreshToken:"${refreshToken}"){
+//       refresh
+//       access
+//     }
+//   }
+// }`,
+//     })
+//     .then((res) => {
+//       return res.data?.access;
+//     })
+//     .catch((err) => {
+//       alert('redirect to login');
+//     });
+// };
+
+// import { useAuth } from '@coop/shared/utils';
+// export const useAxios = <TData, TVariables>(
+//   query: string
+// ): ((
+//   variables?: TVariables,
+//   config?: AxiosRequestConfig<TData>
+// ) => Promise<TData>) => {
+// it is safe to call React Hooks here.
+// const { url, headers } = React.useContext(FetchParamsContext)
+// const url = process.env['NX_SCHEMA_PATH'] ?? '';
+// return axios.post<TData>(url,data:{query,variables}).then((res) => res.data);
+
+// const auth = useAuth();
+
+// const accessToken = auth?.auth?.accessToken;
+// console.log('access', accessToken); // get this token from redux
+
+// return async (variables?: TVariables, config?: AxiosRequestConfig<TData>) => {
+//   const headers = {
+//     Authoirzation: `Bearer ${accessToken}`,
+//   };
+//   if (config) {
+//     if (config.headers) {
+//       config.headers = { ...config.headers, ...headers };
+//     } else {
+//       config.headers = { ...headers };
+//     }
+//   } else {
+//     config = {
+//       headers,
+//     };
+//   }
+//   return axios
+//     .post<{ data: TData }>(url, { query, variables }, config)
+//     .then((res: AxiosResponse<{ data: TData; errors?: any[] }>) => {
+// IF access token is invalid
+// if()
+// refreshToken(url).then((res) => {
+//   console.log('token', res.token);
+//   const headers = {
+//     Authoirzation: `Bearer ${res.token}`,
+//   };
+//   if (config) {
+//     if (config.headers) {
+//       config.headers = { ...config.headers, ...headers };
+//     } else {
+//       config.headers = { ...headers };
+//     }
+//   } else {
+//     config = {
+//       headers,
+//     };
+//   }
+
+//   return axios
+//     .post<{ data: TData }>(url, { query, variables }, config)
+//     .then((res) => {
+//       return res.data.data;
+//     });
+// });
+//         return res.data.data;
+//       });
+//   };
+// };
 
 // // Add a request interceptor
 // axios.interceptors.request.use(
