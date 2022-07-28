3b16a6454651e16468c0bfaa7610fa0e7de518f5
anup-stha <anup.stha012@gmail.com>
2022-07-28 14:27:20 +0545

    fix: build errors

--- libs/cbs/settings/feature-member/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { KYMSettingsDragField } from './components/common/KYMSettingsDragField';
-export { KYMSettingsDragOption } from './components/common/KYMSettingsDragOption';
-export * from './components/general/MemberRiskLevel';
-export * from './components/general/TypeOfMember';
-export { IncomeSourceDetailsAccComponent } from './components/IncomeSourceDetailsAccComponent';
-export * from './lib/KYMIndividualSettingsPage';
--- libs/cbs/settings/feature-members/src/index.ts
+++ libs/cbs/settings/feature-members/src/index.ts
@@ -1,3 +1,5 @@
+export * from './components/general/MemberRiskLevel';
+export * from './components/general/TypeOfMember';
 export * from './lib/CbsSettingsFeatureMembers';
 export * from './pages/IndividualSettings';
 export * from './pages/InstitutionSettings';
