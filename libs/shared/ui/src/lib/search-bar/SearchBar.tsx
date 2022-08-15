f80e05d60bf5ca4bfe0add8ea328f0a04d00ca67
sameergautam01 <100455784+sameergautam01@users.noreply.github.com>
2022-08-15 14:53:57 +0545

    Fix/shortcut search (#413)
    * feat: apex chart implementation
    
    * fix: search bar shortcut fix

--- libs/shared/ui/src/lib/search-bar/SearchBar.tsx
+++ libs/shared/ui/src/lib/search-bar/SearchBar.tsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+import { GlobalHotKeys } from 'react-hotkeys';
 import { IoSearch } from 'react-icons/io5';
 import {
   Icon,
@@ -8,49 +10,69 @@ import {
   InputRightElement,
   Text,
 } from '@chakra-ui/react';
+const keyMap = {
+  inputFocus: ['ctrl+/'],
+  appSwitcher: 'alt+o',
+  showHelpOptions: ['alt+l'],
+
+  // up: ["i"],
+  // shiftUp: ["shift+i"],
+  // delete: ["r"],
+  // addFocus: ["a"]
+};
 
 /* eslint-disable-next-line */
 export interface SearchBarProps extends InputProps {}
 
 export function SearchBar() {
-  return (
-    <InputGroup
-      borderRadius={'6px'}
-      border="none"
-      flex={1}
-      borderColor="secondary.700"
-      color="white"
-      _hover={{ color: 'gray.700' }}
-      _active={{ backgroundColor: 'gray.0' }}
-    >
-      <InputLeftElement
-        pointerEvents="none"
-        children={<Icon as={IoSearch} fontSize="lg" />}
-        _hover={{ color: 'gray.800' }}
-        _active={{ backgrundColor: 'gray.0' }}
-      />
-      <Input
-        type="text"
-        // placeholder="खोज्नुहोस्"
-        placeholder="Search"
-        color={'white'}
-        fontSize="r1"
-        border="none"
-        _active={{ color: 'gray.800', backgroundColor: 'gray.0' }}
-        bg={'secondary.900'}
-        _hover={{ color: 'gray.800', backgroundColor: 'gray.0' }}
-      />
+  const searchBarRef = useRef<HTMLInputElement>(null);
+  const handlers = {
+    inputFocus() {
+      searchBarRef.current?.focus();
+    },
+  };
 
-      <InputRightElement
-        pointerEvents="none"
-        color={'currentcolor'}
-        children={
-          <Text fontSize={'r1'} alignItems="center" pr="s12">
-            Ctrl+/
-          </Text>
-        }
-      />
-    </InputGroup>
+  return (
+    <GlobalHotKeys keyMap={keyMap} handlers={handlers}>
+      <InputGroup
+        borderRadius={'6px'}
+        border="none"
+        flex={1}
+        borderColor="secondary.700"
+        color="white"
+        _hover={{ color: 'gray.700' }}
+        _active={{ backgroundColor: 'gray.0' }}
+      >
+        <InputLeftElement
+          pointerEvents="none"
+          children={<Icon as={IoSearch} fontSize="lg" />}
+          _hover={{ color: 'gray.800' }}
+          _active={{ backgrundColor: 'gray.0' }}
+        />
+        <Input
+          type="text"
+          // placeholder="खोज्नुहोस्"
+          placeholder="Search"
+          color={'white'}
+          fontSize="r1"
+          ref={searchBarRef}
+          border="none"
+          _active={{ color: 'gray.800', backgroundColor: 'gray.0' }}
+          bg={'secondary.900'}
+          _hover={{ color: 'gray.800', backgroundColor: 'gray.0' }}
+        />
+
+        <InputRightElement
+          pointerEvents="none"
+          color={'currentcolor'}
+          children={
+            <Text fontSize={'r1'} alignItems="center" pr="s12">
+              Ctrl+/
+            </Text>
+          }
+        />
+      </InputGroup>
+    </GlobalHotKeys>
   );
 }
 
