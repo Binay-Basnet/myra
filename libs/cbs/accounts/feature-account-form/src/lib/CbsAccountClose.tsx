2212fda7cd578bb6503c8aacb66c153317a6ca90
sameergautam01 <sameer.gautam9860@gmail.com>
2022-08-25 14:10:45 +0545

    feat: account close start

--- libs/cbs/accounts/feature-account-form/src/lib/CbsAccountClose.tsx
+++ libs/cbs/accounts/feature-account-form/src/lib/CbsAccountClose.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable-next-line */
-export interface CbsAccountCloseFormProps {}
+import { Box, Container, FormHeader } from '@coop/shared/ui';
+import { useTranslation } from '@coop/shared/utils';
+import React from 'react';
 
 export function CbsAccountClose() {
+  const { t } = useTranslation();
+
   return (
-    <div>
-      <h1>Welcome to CbsAccountsFeatureAccountForm!</h1>
-    </div>
+    <Container minW="container.xl" p="0" bg="white" h="calc(100vh - 110px)">
+      <Box position="sticky" top="110px" bg="gray.100" width="100%" zIndex="10">
+        <FormHeader title={t['accountClose']} />
+      </Box>
+    </Container>
   );
 }
-
-export default CbsAccountClose;
