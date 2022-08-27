4ceddfd146e53d357f4232326d7786e1a582f7df
Anup Shrestha <anup.stha012@gmail.com>
2022-08-27 08:52:57 +0545

    chore: theme folder refactor

--- libs/shared/utils/src/lib/neosystheme/index.ts
+++ libs/shared/utils/src/lib/neosystheme/index.ts
@@ -1,27 +1,20 @@
-// 1. Import `extendTheme`
 import { extendTheme } from '@chakra-ui/react';
 
 import foundations from './foundations';
-import accordion from './foundations/accordion';
-import button from './foundations/button';
-import checkbox from './foundations/checkbox';
-import Input from './foundations/input';
-import modal from './foundations/modal';
-import radio from './foundations/radio';
-import switches from './foundations/switches';
-import Table from './foundations/table';
-import tags from './foundations/tags';
-
-// 2. Call `extendTheme` and pass your custom values
+import components from '../theme/components';
 
 export const neosysTheme = extendTheme({
+  config: {
+    cssVarPrefix: 'myra',
+  },
   styles: {
     global: () => ({
       'html, body': {
         fontSize: 'md',
-        background: '#EEF2F7',
+        background: 'background.500',
         lineHeight: 'tall',
         scrollBehavior: 'smooth',
+        fontFamily: 'Inter',
       },
       ':not(.chakra-dont-set-collapse) > .chakra-collapse': {
         overflow: 'initial !important',
@@ -58,48 +51,7 @@ export const neosysTheme = extendTheme({
       },
     },
   },
-  components: {
-    Button: {
-      variants: button.variants,
-      sizes: button.sizes,
-      baseStyle: button.baseStyle,
-      defaultProps: button.defaultProps,
-    },
-    Checkbox: {
-      parts: checkbox.parts,
-      sizes: checkbox.sizes,
-      baseStyle: checkbox.baseStyle,
-      defaultProps: checkbox.defaultProps,
-    },
-
-    Radio: {
-      parts: radio.parts,
-      sizes: radio.sizes,
-      baseStyle: radio.baseStyle,
-      defaultProps: radio.defaultProps,
-    },
-    Modal: {
-      parts: modal.parts,
-      sizes: modal.sizes,
-      baseStyle: modal.baseStyle,
-      defaultProps: modal.defaultProps,
-    },
-    Table,
-    Input,
-    Tag: {
-      sizes: tags.sizes,
-      variants: tags.variants,
-      baseStyle: tags.baseStyle,
-      defaultProps: tags.defaultProps,
-    },
-    Switch: {
-      sizes: switches.sizes,
-      baseStyle: switches.baseStyle,
-      defaultProps: switches.defaultProps,
-    },
-    Accordion: {
-      baseStyle: accordion.baseStyle,
-    },
-  },
+  components,
 });
+
 export default neosysTheme;
---
+++ libs/shared/utils/src/lib/theme/components/index.tsx
@@ -0,0 +1,27 @@
+import Accordion from './accordion';
+import Alert from './alert';
+import Button from './button';
+import Checkbox from './checkbox';
+import Input from './input';
+import Modal from './modal';
+import Radio from './radio';
+import Switches from './switches';
+import Table from './table';
+import Tags from './tags';
+import Textarea from './textarea';
+
+export const components = {
+  Accordion,
+  Alert,
+  Button,
+  Checkbox,
+  Input,
+  Modal,
+  Radio,
+  Switches,
+  Table,
+  Tags,
+  Textarea,
+};
+
+export default components;
