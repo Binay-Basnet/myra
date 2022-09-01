04986e3386b378df9bd1ce58fee526ab8ce6f389
anup-stha <anup.stha012@gmail.com>
2022-09-02 00:43:02 +0545

    fix(storybook): update

--- libs/shared/ui/.storybook/main.js
+++ libs/shared/ui/.storybook/main.js
@@ -61,7 +61,7 @@ const {
 module.exports = {
   ...rootMain,
 
-  addons: [...rootAddons],
+  addons: [...rootAddons, '@nrwl/react/plugins/storybook'],
 
   core: { ...rootMain.core, builder: 'webpack5' },
   stories: [
@@ -72,18 +72,6 @@ module.exports = {
 
   webpackFinal: async (config) => {
     config = await rootWebpackFinal(config);
-
-    // Found this here: https://github.com/nrwl/nx/issues/2859
-    // And copied the part of the solution that made it work
-
-    const svgRuleIndex = config.module.rules.findIndex((rule) => {
-      const { test } = rule;
-
-      return test.toString().startsWith('/\\.(svg|ico');
-    });
-
-    config.module.rules[svgRuleIndex].test =
-      /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/;
 
     config.module.rules.push(
       {
