d931d7e3b34368c235ace41257d6a9abc222a225
sameergautam01 <sameer.gautam9860@gmail.com>
2022-09-12 13:07:49 +0545

    fix(cbs): loan page start

--- apps/myra/pages/loan/index.tsx
+++ apps/myra/pages/loan/index.tsx
@@ -5,15 +5,15 @@ import { AccountPagesLayout } from '@coop/myra/components';
 import { MainLayout } from '@coop/shared/ui';
 
 // TODO ( Update this page when design arrives )
-const AccountReportPage = () => {
+const LoanPage = () => {
   return <MemberListPage />;
 };
 
-AccountReportPage.getLayout = function getLayout(page: ReactElement) {
+LoanPage.getLayout = function getLayout(page: ReactElement) {
   return (
     <MainLayout>
       <AccountPagesLayout>{page}</AccountPagesLayout>
     </MainLayout>
   );
 };
-export default AccountReportPage;
+export default LoanPage;
