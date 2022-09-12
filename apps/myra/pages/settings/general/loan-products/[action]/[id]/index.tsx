e8a7bf499f8aaeb9569408628b5b1613146bf900
sky1o1 <aakash.dangol101@gmail.com>
2022-09-12 11:16:10 +0545

    fix(cbs): issue fix

--- apps/myra/pages/settings/general/loan-products/[action]/[id]/index.tsx
+++ apps/myra/pages/settings/general/loan-products/[action]/[id]/index.tsx
@@ -1,9 +1,7 @@
-import { SettingsLoanProductForm } from '@coop/cbs/settings//feature-loan';
+import { SettingsLoanProductForm } from '@coop/cbs/settings/feature-loan';
 import { SettingsLayout } from '@coop/cbs/settings/ui-layout';
 
-const Loan = () => {
-  return <SettingsLoanProductForm />;
-};
+const Loan = () => <SettingsLoanProductForm />;
 
 export default Loan;
 Loan.getLayout = function getLayout(page) {
