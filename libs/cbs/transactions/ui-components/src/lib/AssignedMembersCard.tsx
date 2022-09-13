281aedd6e582fefc184ccc18196cddbc736f383f
ajayprazz <ajayprazz@gmail.com>
2022-09-13 11:14:54 +0545

    update agent overview (#503)
    * feat(cbs): update transaction deposit form
    
    * feat(cbs): update transaction  assigned members card

--- libs/cbs/transactions/ui-components/src/lib/AssignedMembersCard.tsx
+++ libs/cbs/transactions/ui-components/src/lib/AssignedMembersCard.tsx
@@ -1,6 +1,7 @@
+import { useMemo } from 'react';
 import { useRouter } from 'next/router';
 
-import { useGetAgentDetailDataQuery } from '@coop/cbs/data-access';
+import { useGetAgentDetailDataQuery, useGetAgentTodayListDataQuery } from '@coop/cbs/data-access';
 import { Box, Button, Grid, Text } from '@coop/shared/ui';
 
 export const AssignedMembersCard = () => {
@@ -13,48 +14,41 @@ export const AssignedMembersCard = () => {
     { enabled: !!id }
   );
 
+  const { data: agentTodayListQueryData } = useGetAgentTodayListDataQuery(
+    {
+      id: id as string,
+    },
+    { staleTime: 0 }
+  );
+
+  const todaysCount = useMemo(
+    () => agentTodayListQueryData?.transaction?.listAgentTask?.record?.length ?? 0,
+    [agentTodayListQueryData]
+  );
+
   return (
-    <Grid
-      p="s16"
-      bg="white"
-      borderRadius="br2"
-      templateColumns="repeat(3, 1fr)"
-      columnGap="s20"
-    >
-      <Box
-        display="flex"
-        flexDirection="column"
-        gap="s4"
-        alignItems="flex-start"
-      >
+    <Grid p="s16" bg="white" borderRadius="br2" templateColumns="repeat(3, 1fr)" columnGap="s20">
+      <Box display="flex" flexDirection="column" gap="s4" alignItems="flex-start">
         <Text fontSize="s3" fontWeight={500} color="neutralColorLight.Gray-80">
           Total Assigned Members
         </Text>
         <Text fontSize="l1" fontWeight={500} color="neutralColorLight.Gray-60">
-          {agentDetailQueryData?.transaction?.agentDetail?.data?.totalMembers ??
-            0}
+          {agentDetailQueryData?.transaction?.agentDetail?.data?.totalMembers ?? 0}
         </Text>
 
         <Button
           variant="link"
-          onClick={() =>
-            router.push(`/transactions/agent/${id}/assigned-members`)
-          }
+          onClick={() => router.push(`/transactions/agent/${id}/assigned-members`)}
         >
           View All Members
         </Button>
       </Box>
-      <Box
-        display="flex"
-        flexDirection="column"
-        gap="s4"
-        alignItems="flex-start"
-      >
+      <Box display="flex" flexDirection="column" gap="s4" alignItems="flex-start">
         <Text fontSize="s3" fontWeight={500} color="neutralColorLight.Gray-80">
-          Today's Member List
+          Today&apos;s Member List
         </Text>
         <Text fontSize="l1" fontWeight={500} color="neutralColorLight.Gray-60">
-          0
+          {todaysCount}
         </Text>
 
         {/* <Button variant="link" disabled>
