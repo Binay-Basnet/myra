24e06ce164daae0facdfb2f0c53481c1584c6805
Suyash <suyashshakya.h0p3@gmail.com>
2022-09-19 22:01:11 +0545

    fix(auth): auth fixes

--- apps/myra/pages/_app.tsx
+++ apps/myra/pages/_app.tsx
@@ -9,7 +9,7 @@ import Script from 'next/script';
 import { ChakraProvider, createStandaloneToast } from '@chakra-ui/react';
 
 import { Box, FloatingShortcutButton, Toaster } from '@coop/shared/ui';
-import { store, theme, useSnap } from '@coop/shared/utils';
+import { store, theme, useInit, useSnap } from '@coop/shared/utils';
 
 import '@raralabs/web-feedback/dist/css/style.css'; // stylesheet
 import 'react-patro/src/styles.css';
