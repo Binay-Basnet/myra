52146554947d2f76180f333939aacd05bdfbab84
Anup Shrestha <anup.stha012@gmail.com>
2022-10-19 10:48:23 +0545

    feat(ebanking): complete signup validations (#633)

--- libs/ebanking/feature-auth/src/lib/LoginPage.tsx
+++ libs/ebanking/feature-auth/src/lib/LoginPage.tsx
@@ -50,64 +50,66 @@ export const LoginPage = () => {
   });
 
   return (
-    <AuthContainer
-      title="Welcome back to Myra!"
-      subtitle="Please enter your login credentials below to continue using the app."
-    >
-      <Box display="flex" flexDir="column" gap="s16">
-        <Box display="flex" flexDir="column" gap="s20">
-          <Input
-            leftElement={<Icon as={AiOutlineMobile} color="gray.500" />}
-            label="Mobile Number"
-            placeholder="Enter your mobile number"
-            {...register('mobileNo')}
-          />
-          <PasswordInput
-            placeholder="Enter your password"
-            fieldName="password"
-            register={register as unknown as UseFormRegister<FieldValues>}
-          />
-        </Box>
-        <Box display="flex" alignItems="center" justifyContent="space-between">
-          <Checkbox label="Remember Password" />
-          <Box fontStyle="italic" color="primary.500" fontSize="s2" cursor="pointer">
-            Forget Password?
+    <form onSubmit={onSubmit}>
+      <AuthContainer
+        title="Welcome back to Myra!"
+        subtitle="Please enter your login credentials below to continue using the app."
+      >
+        <Box display="flex" flexDir="column" gap="s16">
+          <Box display="flex" flexDir="column" gap="s20">
+            <Input
+              leftElement={<Icon as={AiOutlineMobile} color="gray.500" />}
+              label="Mobile Number"
+              placeholder="Enter your mobile number"
+              {...register('mobileNo')}
+            />
+            <PasswordInput
+              placeholder="Enter your password"
+              fieldName="password"
+              register={register as unknown as UseFormRegister<FieldValues>}
+            />
+          </Box>
+          <Box display="flex" alignItems="center" justifyContent="space-between">
+            <Checkbox label="Remember Password" />
+            <Box fontStyle="italic" color="primary.500" fontSize="s2" cursor="pointer">
+              Forget Password?
+            </Box>
           </Box>
         </Box>
-      </Box>
 
-      <Box w="100%" display="flex" flexDir="column" gap="s16">
-        <Button h="s48" w="100%" onClick={onSubmit} isLoading={isLoading}>
-          Log In
-        </Button>
-        <Text fontSize="r1" color="gray.700">
-          Don&apos;t have an account?{' '}
-          <Text
-            as="span"
-            color="primary.500"
-            cursor="pointer"
-            onClick={() => router.push('/sign-up')}
-          >
-            Create new Myra account
-          </Text>
-        </Text>
-      </Box>
-
-      <Box pt="s40" display="flex" flexDir="column" gap="s16">
-        <Box display="flex" alignItems="center" gap="s16">
-          <Box h="1px" bg="#DCDCDC" w="100%" />
-          <Text fontSize="r1" color="gray.500">
-            OR
-          </Text>
-          <Box h="1px" bg="#DCDCDC" w="100%" />
-        </Box>
-        <Box>
-          <Button h="s48" w="100%" variant="outline">
-            Apply for COOP registration
+        <Box w="100%" display="flex" flexDir="column" gap="s16">
+          <Button h="s48" w="100%" type="submit" isLoading={isLoading}>
+            Log In
           </Button>
+          <Text fontSize="r1" color="gray.700">
+            Don&apos;t have an account?{' '}
+            <Text
+              as="span"
+              color="primary.500"
+              cursor="pointer"
+              onClick={() => router.push('/sign-up')}
+            >
+              Create new Myra account
+            </Text>
+          </Text>
         </Box>
-      </Box>
-    </AuthContainer>
+
+        <Box pt="s40" display="flex" flexDir="column" gap="s16">
+          <Box display="flex" alignItems="center" gap="s16">
+            <Box h="1px" bg="#DCDCDC" w="100%" />
+            <Text fontSize="r1" color="gray.500">
+              OR
+            </Text>
+            <Box h="1px" bg="#DCDCDC" w="100%" />
+          </Box>
+          <Box>
+            <Button h="s48" w="100%" variant="outline" type="button">
+              Apply for COOP registration
+            </Button>
+          </Box>
+        </Box>
+      </AuthContainer>
+    </form>
   );
 };
 
