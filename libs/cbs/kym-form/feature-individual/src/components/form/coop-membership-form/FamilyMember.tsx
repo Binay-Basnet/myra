e9fb5c63d4b10a41d22070dd6512c9f429355160
ajayprazz <ajayprazz@gmail.com>
2022-11-09 16:04:56 +0545

    feat(kym): update family member remove section (#711)

--- libs/cbs/kym-form/feature-individual/src/components/form/coop-membership-form/FamilyMember.tsx
+++ libs/cbs/kym-form/feature-individual/src/components/form/coop-membership-form/FamilyMember.tsx
@@ -1,10 +1,5 @@
 import { useEffect } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
-import { BsFillTelephoneFill } from 'react-icons/bs';
-import { GrMail } from 'react-icons/gr';
-import { IoLocationSharp } from 'react-icons/io5';
-import { RiShareBoxFill } from 'react-icons/ri';
-import { CloseIcon } from '@chakra-ui/icons';
 import debounce from 'lodash/debounce';
 
 import {
@@ -13,20 +8,10 @@ import {
   useGetIndividualKymOptionsQuery,
   useSetMemberFamilyDetailsMutation,
 } from '@coop/cbs/data-access';
+import { InputGroupContainer } from '@coop/cbs/settings/ui-containers';
 import { FormSelect } from '@coop/shared/form';
-import {
-  Avatar,
-  Box,
-  Grid,
-  GridItem,
-  Icon,
-  Text,
-  TextFields,
-} from '@coop/shared/ui';
-import {
-  useGetIndividualMemberDetails,
-  useTranslation,
-} from '@coop/shared/utils';
+import { Box, Button, MemberCard } from '@coop/shared/ui';
+import { useGetIndividualMemberDetails, useTranslation } from '@coop/shared/utils';
 
 import { getFieldOption } from '../../../utils/getFieldOption';
 
@@ -49,22 +34,22 @@ export const FamilyMember = ({
 
   const { watch, reset } = methods;
 
-  const { memberDetailData } = useGetIndividualMemberDetails({
-    memberId: familyMemberId as string,
-  });
+  const { memberDetailData, memberCitizenshipUrl, memberSignatureUrl } =
+    useGetIndividualMemberDetails({
+      memberId: familyMemberId as string,
+    });
 
   const { data: familyRelationShipData, isLoading: familyRelationshipLoading } =
     useGetIndividualKymOptionsQuery({
       searchTerm: FormFieldSearchTerm.Relationship,
     });
 
-  const { data: familyMemberListQueryData } =
-    useGetIndividualKymFamilyMembersListQuery(
-      {
-        id: String(memberId),
-      },
-      { enabled: !!memberId }
-    );
+  const { data: familyMemberListQueryData } = useGetIndividualKymFamilyMembersListQuery(
+    {
+      id: String(memberId),
+    },
+    { enabled: !!memberId }
+  );
 
   useEffect(() => {
     const familyMemberData =
@@ -105,174 +90,47 @@ export const FamilyMember = ({
         display="flex"
         flexDirection="column"
       >
-        <Box
-          bg="background.500"
-          borderBottom="1px solid"
-          borderColor="border.layout"
-        >
-          <Grid
-            templateRows="repeat(1,1fr)"
-            templateColumns="repeat(5,1fr)"
-            gap={2}
-            mt="s20"
-            mb="s20"
-            ml="s16"
-          >
-            <GridItem display="flex" alignSelf="center" colSpan={2}>
-              <Box m="10px">
-                <Avatar
-                  src="https://www.kindpng.com/picc/m/483-4834603_daniel-hudson-passport-size-photo-bangladesh-hd-png.png"
-                  size="lg"
-                  // name={data?.personalInformation?.name?.firstName}
-                  name={memberDetailData?.name}
-                />
-              </Box>
-              <Box>
-                <TextFields
-                  color="neutralColorLight.Gray-80"
-                  fontWeight="Medium"
-                  fontSize="s3"
-                >
-                  {memberDetailData?.name}
-                </TextFields>
-                <Text
-                  color="neutralColorLight.Gray-80"
-                  fontSize="s3"
-                  fontWeight="Regular"
-                >
-                  {/* ID: {data?.personalInformation?.panNumber} */}
-                  {t['id']} : {memberDetailData?.id}
-                </Text>
-
-                <Text
-                  color="neutralColorLight.Gray-60"
-                  fontWeight="Regular"
-                  fontSize="s3"
-                >
-                  {/* Member Since: {data?.personalInformation?.dateOfBirth} */}
-                  {t['memberSince']} : {memberDetailData?.dateJoined}
-                </Text>
-
-                <Text
-                  color="neutralColorLight.Gray-60"
-                  fontWeight="Regular"
-                  fontSize="s3"
-                >
-                  {/* Branch: {data?.address?.temporary?.state} */}
-                  ABC SACCOS
-                </Text>
-              </Box>
-            </GridItem>
-
-            <GridItem
-              display="flex"
-              flexDirection="column"
-              alignSelf="center"
-              colSpan={2}
-              gap={3}
-            >
-              <Box display="flex">
-                <Icon size="sm" as={BsFillTelephoneFill} color="primary.500" />
-                <TextFields
-                  ml="10px"
-                  color="neutralColorLight.Gray-80"
-                  fontSize="s3"
-                  fontWeight="Regular"
-                >
-                  {/* {data?.contact?.mobile} */}
-                  {memberDetailData?.contact}
-                </TextFields>
-              </Box>
-
-              <Box display="flex">
-                <Icon size="sm" as={GrMail} color="primary.500" />
-                <TextFields
-                  ml="10px"
-                  color="neutralColorLight.Gray-80"
-                  fontSize="s3"
-                  fontWeight="Regular"
-                >
-                  {memberDetailData?.email}
-                </TextFields>
-              </Box>
-
-              <Box display="flex">
-                <Icon size="sm" as={IoLocationSharp} color="primary.500" />
-                <TextFields
-                  ml="10px"
-                  color="neutralColorLight.Gray-80"
-                  fontSize="s3"
-                  fontWeight="Regular"
-                >
-                  {memberDetailData?.address}
-                </TextFields>
-              </Box>
-            </GridItem>
-
-            <GridItem
-              mr="s16"
-              display="flex"
-              flexDirection="column"
-              justifyContent="space-between"
-            >
-              <Box alignSelf="flex-end">
-                <Icon
-                  h="14px"
-                  w="14px"
-                  as={CloseIcon}
-                  color="gray.500"
-                  cursor="pointer"
-                  onClick={() => removeFamilyMember(mutationId)}
-                />
-              </Box>
-              <Box display="flex" alignSelf="flex-end">
-                <Text
-                  fontWeight="Medium"
-                  color="primary.500"
-                  fontSize="s2"
-                  mr="5px"
-                >
-                  {t['kynIndViewProfile']}
-                </Text>
-                <Icon size="sm" as={RiShareBoxFill} color="primary.500" />
-              </Box>
-            </GridItem>
-          </Grid>
+        <Box borderBottom="1px solid" borderColor="border.layout">
+          <MemberCard
+            isInline
+            memberDetails={{
+              name: memberDetailData?.name,
+              avatar: memberDetailData?.profilePicUrl ?? '',
+              memberID: memberDetailData?.id,
+              gender: memberDetailData?.gender,
+              age: memberDetailData?.age,
+              maritalStatus: memberDetailData?.maritalStatus,
+              dateJoined: memberDetailData?.dateJoined,
+              phoneNo: memberDetailData?.contact,
+              email: memberDetailData?.email,
+              address: memberDetailData?.address,
+            }}
+            signaturePath={memberSignatureUrl}
+            showSignaturePreview={false}
+            citizenshipPath={memberCitizenshipUrl}
+          />
         </Box>
 
-        <Grid p="s16" bg="background.500" templateColumns="repeat(2,1fr)">
-          <GridItem>
-            <Text
-              fontWeight="Regular"
-              fontSize="s3"
-              color="neutralColorLight.gray-80"
-            >
-              {t['kynIndCitizenshipNo']} : {memberDetailData?.citizenship?.idNo}
-            </Text>
-          </GridItem>
-          <GridItem>
-            <Text
-              fontWeight="Regular"
-              fontSize="s3"
-              color="neutralColorLight.gray-80"
-            >
-              {t['kynIndPresentAddress']} : {memberDetailData?.address}
-            </Text>
-          </GridItem>
-        </Grid>
-        <Box px="s16" py="s32" w="40%">
+        <Box p="s16" borderBottom="1px solid" borderColor="border.layout">
           <FormProvider {...methods}>
             <form>
-              <FormSelect
-                name={`relationshipId`}
-                id="familyMemberInThisCooperative"
-                label={t['kymIndRelationship']}
-                __placeholder={t['kymIndSelectRelationship']}
-                isLoading={familyRelationshipLoading}
-                options={getFieldOption(familyRelationShipData)}
-              />
+              <InputGroupContainer>
+                <FormSelect
+                  name="relationshipId"
+                  id="familyMemberInThisCooperative"
+                  label={t['kymIndRelationship']}
+                  isLoading={familyRelationshipLoading}
+                  options={getFieldOption(familyRelationShipData)}
+                />
+              </InputGroupContainer>
             </form>
           </FormProvider>
+        </Box>
+
+        <Box p="s16" display="flex" justifyContent="flex-end">
+          <Button variant="ghost" shade="danger" onClick={() => removeFamilyMember(mutationId)}>
+            Remove
+          </Button>
         </Box>
       </Box>
     </Box>
