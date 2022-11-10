37b670a6265ced4b5927e27567405e16363cdcbf
Anup Shrestha <anup.stha012@gmail.com>
2022-11-10 11:38:03 +0545

    feat(coa): complete delete functionality in coa

--- libs/cbs/settings/feature-coa/src/components/Tree/LeafNode.tsx
+++ libs/cbs/settings/feature-coa/src/components/Tree/LeafNode.tsx
@@ -1,8 +1,10 @@
 import { MdOutlineCircle } from 'react-icons/md';
+import { useQueryClient } from 'react-query';
 import { useRouter } from 'next/router';
-import { AddIcon } from '@chakra-ui/icons';
+import { AddIcon, DeleteIcon } from '@chakra-ui/icons';
 
-import { Box, Button, Icon, Text } from '@coop/shared/ui';
+import { useDeleteCoaMutation } from '@coop/cbs/data-access';
+import { asyncToast, Box, Button, Icon, Text } from '@coop/shared/ui';
 
 import NodeWrapper from './NodeWrapper';
 import { CoaTree } from '../../types';
@@ -11,25 +13,22 @@ interface ITestProps {
   data: CoaTree;
 }
 
-function LeafNode(props: ITestProps) {
+const LeafNode = ({ data }: ITestProps) => {
   const router = useRouter();
+
+  const queryClient = useQueryClient();
+  const { mutateAsync: deleteCOA } = useDeleteCoaMutation();
 
   return (
     <NodeWrapper>
-      <Box
-        display="flex"
-        gap="s8"
-        ml="-3px"
-        alignItems="center"
-        cursor="pointer"
-      >
+      <Box display="flex" gap="s8" ml="-3px" alignItems="center" cursor="pointer">
         <Icon size="sm" as={MdOutlineCircle} color="gray.500" />
         <Box display="flex" alignItems="center" gap="s8" role="group">
           <Text fontWeight="bold" fontSize="14px">
-            {props.data.accountCode}
+            {data.accountCode}
           </Text>
           <Text fontWeight="400" fontSize="14px">
-            {props.data.name?.local}
+            {data.name?.local}
           </Text>
           <Button
             variant="link"
@@ -41,17 +40,42 @@ function LeafNode(props: ITestProps) {
             _groupHover={{ display: 'flex' }}
             onClick={() =>
               router.push(
-                `/settings/general/charts-of-accounts/add-new-account?under=${props.data.accountCode}`
+                `/settings/general/charts-of-accounts/add-new-account?under=${data.accountCode}`
               )
             }
           >
             <Icon as={AddIcon} w="14px" h="14px" />
             Add Account
           </Button>
+          {data?.category === 'USER_DEFINED' ? (
+            <Button
+              variant="link"
+              py="0"
+              lineHeight="0"
+              shade="danger"
+              gap="s8"
+              display="none"
+              _groupHover={{ display: 'flex' }}
+              onClick={async () => {
+                await asyncToast({
+                  id: 'delete-coa',
+                  msgs: {
+                    loading: 'Deleting',
+                    success: 'Deleted Successfully',
+                  },
+                  promise: deleteCOA({ id: data?.id as string }),
+                  onSuccess: () => queryClient.invalidateQueries('getCoaFullView'),
+                });
+              }}
+            >
+              <Icon as={DeleteIcon} w="14px" h="14px" />
+              Delete Account
+            </Button>
+          ) : null}
         </Box>
       </Box>
     </NodeWrapper>
   );
-}
+};
 
 export default LeafNode;
