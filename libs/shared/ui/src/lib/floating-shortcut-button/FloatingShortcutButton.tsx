ceb0dfc849bbf3301f4ffc366676bcd5bcaaf176
Suyash <suyashshakya.h0p3@gmail.com>
2022-11-11 17:08:54 +0545

    fix(new): whats new changes

--- libs/shared/ui/src/lib/floating-shortcut-button/FloatingShortcutButton.tsx
+++ libs/shared/ui/src/lib/floating-shortcut-button/FloatingShortcutButton.tsx
@@ -127,7 +127,7 @@ const WhatsNewModal = (props: WhatsNewModalProps) => {
       <Box p={3} w="100%" display="flex" flexDirection="column" gap={5}>
         <Box display="flex" justifyContent="space-between">
           <Text fontSize="r2">Version 1.0.5</Text>
-          <Text fontSize="s3">November 10, 2022</Text>
+          <Text fontSize="s3">November 11, 2022</Text>
         </Box>
         <Box>
           <Box display="flex" alignItems="center" gap={2}>
