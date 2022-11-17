bb0c6418f850e1322e50a0bdff1bf73caed0954d
Anup Shrestha <anup.stha012@gmail.com>
2022-11-17 15:36:47 +0545

    feat(project): seperate db slug in axios helpers

--- libs/ebanking/data-access/src/generated/myra/axiosHelper.ts
+++ libs/ebanking/data-access/src/generated/myra/axiosHelper.ts
@@ -5,17 +5,19 @@ import { getSchemaPath } from '@coop/shared/utils';
 
 import { RootState, useAppSelector } from '../../redux/store';
 
-// // Request interceptors for API calls
-// axios.interceptors.request.use(
-//   (config) => {
-//     config.headers = {
-//       ...config.headers,
-//       slug: typeof window !== 'undefined' ? window.location.host.split('.')[0] : '',
-//     };
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
+const axiosAgent = axios.create();
+
+// Request interceptors for API calls
+axiosAgent.interceptors.request.use(
+  (config) => {
+    config.headers = {
+      ...config.headers,
+      slug: 'neosys',
+    };
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export const useAxios = <TData, TVariables>(
   query: string
@@ -46,7 +48,7 @@ export const useAxios = <TData, TVariables>(
       config = { headers };
     }
 
-    return axios
+    return axiosAgent
       .post<{ data: TData }>(url, { query, variables }, config)
       .then(
         (
@@ -83,7 +85,7 @@ export const useAxios = <TData, TVariables>(
               config = { headers };
             }
 
-            return axios.post<{ data: TData }>(url, { query, variables }, config).then(
+            return axiosAgent.post<{ data: TData }>(url, { query, variables }, config).then(
               (
                 res: AxiosResponse<{
                   data: TData;
@@ -105,4 +107,4 @@ export const useAxios = <TData, TVariables>(
   };
 };
 
-axios.interceptors.response.use((response) => response);
+axiosAgent.interceptors.response.use((response) => response);
