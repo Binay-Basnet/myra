e52d8b65550215b00ebd2ff01610c927a8a6772f
Suyash <suyashshakya.h0p3@gmail.com>
2022-12-08 10:18:21 +0545

    fix(temp): temp update

--- apps/myra/pages/temp.tsx
+++ apps/myra/pages/temp.tsx
@@ -1,21 +1,5 @@
-import { EditableTable } from '@myra-ui/editable-table';
+import { Box } from '@myra-ui';
 
-import { COASelectModal } from '@coop/shared/components';
-
-const Temp = () => (
-  <EditableTable
-    debug
-    defaultData={[{ ledger: '20.1' }]}
-    columns={[
-      {
-        accessor: 'ledger',
-        cellWidth: 'auto',
-        header: 'Ledger',
-        fieldType: 'modal',
-        modal: COASelectModal,
-      },
-    ]}
-  />
-);
+const Temp = () => <Box />;
 
 export default Temp;
