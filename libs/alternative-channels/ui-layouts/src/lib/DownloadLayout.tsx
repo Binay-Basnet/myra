702592245fdabd29b7aef00072286298c9a72d4b
sameergautam01 <sameer.gautam9860@gmail.com>
2022-12-22 15:25:36 +0545

    feat(layout): alternative channel sidebar

--- libs/alternative-channels/ui-layouts/src/lib/DownloadLayout.tsx
+++ libs/alternative-channels/ui-layouts/src/lib/DownloadLayout.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import { Box, Divider, Text } from '@myra-ui';
+import { Sidebar } from '@myra-ui';
 
-import { TabColumn } from '@coop/myra/components';
 import { useTranslation } from '@coop/shared/utils';
 
 interface IAccountPageLayoutProps {
@@ -32,27 +31,12 @@ export const DownloadLayout = ({ children }: IAccountPageLayoutProps) => {
   const { t } = useTranslation();
 
   return (
-    <Box display="flex">
-      <Box width="260px" flexShrink={0} position="fixed">
-        <Box height="60px" py="s12" px="s16">
-          <Text fontSize="s2" fontWeight="600" color="primary.500">
-            Alternative Channels
-          </Text>
-          <Text fontSize="l1" fontWeight="600" color="gray.800">
-            {t['acDownload']}
-          </Text>
-        </Box>
-        <Box p="s16">
-          <Divider my="s16" />
-          <TabColumn list={userColumns} />
-          <Divider my="s16" />
-        </Box>
-      </Box>
-      <Box width="calc(100% - 260px)" marginLeft="260px">
-        <Box bg="white" minHeight="calc(100vh - 110px)">
-          {children}
-        </Box>
-      </Box>
-    </Box>
+    <Sidebar
+      applicationName="Alternative Channels"
+      featureName={t['acDownload']}
+      featureLink="/alternative-channels/downloads/forms"
+      tabColumns={userColumns}
+      children={children}
+    />
   );
 };
--- libs/alternative-channels/ui-layouts/src/lib/UserLayout.tsx
+++ libs/alternative-channels/ui-layouts/src/lib/UserLayout.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { useRouter } from 'next/router';
 
-import { AddButtonList, Box, Divider, PopOverComponentForButtonList, Text } from '@myra-ui';
+import { Sidebar } from '@myra-ui';
 
-import { TabColumn } from '@coop/myra/components';
 import { useTranslation } from '@coop/shared/utils';
 
 interface IAccountPageLayoutProps {
@@ -44,39 +42,17 @@ const addButtoncolumns = [
 ];
 
 export const UserLayout = ({ children }: IAccountPageLayoutProps) => {
-  const router = useRouter();
   const { t } = useTranslation();
 
   return (
-    <Box display="flex">
-      <Box width="260px" flexShrink={0} position="fixed">
-        <Box height="60px" py="s12" px="s16">
-          <Text fontSize="s2" fontWeight="600" color="primary.500">
-            Alternative Channels
-          </Text>
-          <Text fontSize="l1" fontWeight="600" color="gray.800">
-            {t['acUsers']}
-          </Text>
-        </Box>
-        <Box p="s16">
-          <PopOverComponentForButtonList buttonLabel="shareLayoutNewShare">
-            {addButtoncolumns.map((item) => (
-              <Box key={item?.title}>
-                <AddButtonList label={t[item.title]} onClick={() => router.push(`${item.link}`)} />
-              </Box>
-            ))}
-          </PopOverComponentForButtonList>
-
-          <Divider my="s16" />
-          <TabColumn list={userColumns} />
-          <Divider my="s16" />
-        </Box>
-      </Box>
-      <Box width="calc(100% - 260px)" marginLeft="260px">
-        <Box bg="white" minHeight="calc(100vh - 110px)">
-          {children}
-        </Box>
-      </Box>
-    </Box>
+    <Sidebar
+      applicationName="Alternative Channels"
+      featureName={t['acUsers']}
+      featureLink="/alternative-channels/users/mBanking"
+      mainButtonLabel={t['shareLayoutNewShare']}
+      tabColumns={userColumns}
+      addButtonList={addButtoncolumns}
+      children={children}
+    />
   );
 };
