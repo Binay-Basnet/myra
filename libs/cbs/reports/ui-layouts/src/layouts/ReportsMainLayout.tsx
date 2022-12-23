a5f0b214580e3b64351c3f4c35fe8ba9885a2e67
Anup Shrestha <anup.stha012@gmail.com>
2022-12-23 10:41:28 +0545

    fix(report): layout fixes

--- libs/cbs/reports/ui-layouts/src/layouts/ReportsMainLayout.tsx
+++ libs/cbs/reports/ui-layouts/src/layouts/ReportsMainLayout.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import Link from 'next/link';
 
-import { Box, Sidebar, Text } from '@myra-ui';
+import { Sidebar } from '@myra-ui';
 
-import { TabColumn } from '@coop/myra/components';
 import { useTranslation } from '@coop/shared/utils';
 
 interface IMemberPageLayout {
@@ -25,46 +23,12 @@ export const ReportMainLayout = ({ children }: IMemberPageLayout) => {
   const { t } = useTranslation();
 
   return (
-    <>
-      <Box display="flex">
-        <Box width="260px" flexShrink={0} position="fixed" zIndex={1}>
-          <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="start"
-            py="s16"
-            pb="s8"
-            justifyContent="center"
-            gap="s2"
-            px="s16"
-          >
-            <Text fontSize="s2" fontWeight="600" color="primary.500">
-              {t['corebankingSystems']}
-            </Text>
-
-            <Link href="/reports/cbs/organizations">
-              <Text lineHeight="125%" fontSize="l1" fontWeight="600" color="gray.800">
-                {t['reportsHeading']}
-              </Text>
-            </Link>
-          </Box>
-
-          <Box p="s16">
-            <TabColumn list={reportColumns} />
-          </Box>
-        </Box>
-        <Box width="100%" ml="260px" bg="white" minHeight="calc(100vh - 110px)">
-          {children}
-        </Box>
-      </Box>
-      <Sidebar
-        applicationName={t['corebankingSystems']}
-        featureName={t['reportsHeading']}
-        featureLink="/reports/cbs/organizations"
-        mainButtonLabel={t['newTransfer']}
-        tabColumns={reportColumns}
-        children={children}
-      />
-    </>
+    <Sidebar
+      applicationName={t['corebankingSystems']}
+      featureName={t['reportsHeading']}
+      featureLink="/reports/cbs/organizations"
+      tabColumns={reportColumns}
+      children={children}
+    />
   );
 };
