c379d9a8558a12dab0c3a46fd32e6ad52cc2b3b7
Aakash Dangol <34686808+sky1o1@users.noreply.github.com>
2022-12-29 12:45:49 +0545

    fix(cbs): issue fix (#981)

--- libs/cbs/kym-form/feature-cooperative-union/src/components/FormSections/economic-details/assets.tsx
+++ libs/cbs/kym-form/feature-cooperative-union/src/components/FormSections/economic-details/assets.tsx
@@ -1,12 +1,16 @@
+import { useEffect } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
 import { Box, Divider, Grid, GridItem, Text } from '@chakra-ui/react';
+import { isNaN } from 'lodash';
 
-import { CoopUnionEconomicDetailsInput } from '@coop/cbs/data-access';
-import { FormInput } from '@coop/shared/form';
 import {
-  getKymSectionCoOperativeUnion,
-  useTranslation,
-} from '@coop/shared/utils';
+  CoopUnionEconomicDetailsInput,
+  setCooperativeUnionTotalAssetsCurrent,
+  setCooperativeUnionTotalAssetsTarget,
+} from '@coop/cbs/data-access';
+import { FormInput } from '@coop/shared/form';
+import { getKymSectionCoOperativeUnion, useTranslation } from '@coop/shared/utils';
 
 import { useCooperativeUnionEconomicDetails } from '../../../hooks';
 
@@ -14,10 +18,9 @@ interface IKymAssestsAndtargetProps {
   setSection: (section?: { section: string; subSection: string }) => void;
 }
 
-export const KymAssestsAndtarget = ({
-  setSection,
-}: IKymAssestsAndtargetProps) => {
+export const KymAssestsAndtarget = ({ setSection }: IKymAssestsAndtargetProps) => {
   const { t } = useTranslation();
+  const dispatch = useDispatch();
 
   const methods = useForm<CoopUnionEconomicDetailsInput>();
 
@@ -30,8 +33,7 @@ export const KymAssestsAndtarget = ({
   const bankCurrent = watch('bankCurrent');
   const bankCurrent2 = isNaN(bankCurrent) === true ? 0 : bankCurrent;
   const investemntCurrent = watch('investmentsCurrent');
-  const investmentCurrent2 =
-    isNaN(investemntCurrent) === true ? 0 : investemntCurrent;
+  const investmentCurrent2 = isNaN(investemntCurrent) === true ? 0 : investemntCurrent;
   const loanCurrent = watch('loanCurrent');
   const loanCurrent2 = isNaN(loanCurrent) === true ? 0 : loanCurrent;
   const nonCurrentAssetsCurrent = watch('nonCurrentAssetsCurrent');
@@ -39,9 +41,20 @@ export const KymAssestsAndtarget = ({
     isNaN(nonCurrentAssetsCurrent) === true ? 0 : nonCurrentAssetsCurrent;
   const otherNonCurrentAssetsCurrent = watch('otherNonCurrentAssetsCurrent');
   const otherNonCurrentAssetsCurrent2 =
-    isNaN(otherNonCurrentAssetsCurrent) === true
-      ? 0
-      : otherNonCurrentAssetsCurrent;
+    isNaN(otherNonCurrentAssetsCurrent) === true ? 0 : otherNonCurrentAssetsCurrent;
+
+  const cashTarget = isNaN(watch('cashAndCashEquivalentTarget'))
+    ? 0
+    : watch('cashAndCashEquivalentTarget');
+  const bankTarget = isNaN(watch('bankTarget')) ? 0 : watch('bankTarget');
+  const investmentsTarget = isNaN(watch('investmentsTarget')) ? 0 : watch('investmentsTarget');
+  const loanTarget = isNaN(watch('loanTarget')) ? 0 : watch('loanTarget');
+  const currentAssetsTarget = isNaN(watch('nonCurrentAssetsTarget'))
+    ? 0
+    : watch('nonCurrentAssetsTarget');
+  const nonCurrentAssetsTarget = isNaN(watch('otherNonCurrentAssetsTarget'))
+    ? 0
+    : watch('otherNonCurrentAssetsTarget');
 
   const totalassestscurrent =
     Number(cashCurrent2) +
@@ -51,21 +64,6 @@ export const KymAssestsAndtarget = ({
     Number(nonCurrentAssetsCurrent2) +
     Number(otherNonCurrentAssetsCurrent2);
 
-  const cashTarget = isNaN(watch('cashAndCashEquivalentTarget'))
-    ? 0
-    : watch('cashAndCashEquivalentTarget');
-  const bankTarget = isNaN(watch('bankTarget')) ? 0 : watch('bankTarget');
-  const investmentsTarget = isNaN(watch('investmentsTarget'))
-    ? 0
-    : watch('investmentsTarget');
-  const loanTarget = isNaN(watch('loanTarget')) ? 0 : watch('loanTarget');
-  const currentAssetsTarget = isNaN(watch('nonCurrentAssetsTarget'))
-    ? 0
-    : watch('nonCurrentAssetsTarget');
-  const nonCurrentAssetsTarget = isNaN(watch('otherNonCurrentAssetsTarget'))
-    ? 0
-    : watch('otherNonCurrentAssetsTarget');
-
   const totalassestsTARGET =
     Number(cashTarget) +
     Number(bankTarget) +
@@ -73,6 +71,11 @@ export const KymAssestsAndtarget = ({
     Number(loanTarget) +
     Number(currentAssetsTarget) +
     Number(nonCurrentAssetsTarget);
+
+  useEffect(() => {
+    dispatch(setCooperativeUnionTotalAssetsCurrent(totalassestscurrent));
+    dispatch(setCooperativeUnionTotalAssetsTarget(totalassestsTARGET));
+  }, [dispatch, totalassestscurrent, totalassestsTARGET]);
 
   return (
     <FormProvider {...methods}>
@@ -87,47 +90,27 @@ export const KymAssestsAndtarget = ({
           display="flex"
           flexDirection="column"
           id="kymCoopUnionAccAssets"
-          scrollMarginTop={'200px'}
+          scrollMarginTop="200px"
           mt="s32"
           pb="s20"
           borderBottom="1px solid"
           borderBottomColor="border.layout"
         >
-          <Grid
-            columnGap={10}
-            alignItems="center"
-            px="s14"
-            templateColumns="repeat(3,1fr)"
-          >
+          <Grid columnGap={10} alignItems="center" px="s14" templateColumns="repeat(3,1fr)">
             <GridItem>
-              <Text
-                mb="s16"
-                color="neutralColorLight.Gray-80"
-                fontSize="s3"
-                fontWeight="SemiBold"
-              >
+              <Text mb="s16" color="neutralColorLight.Gray-80" fontSize="s3" fontWeight="SemiBold">
                 {t['kymCoopUnionEcoAssets']}
               </Text>
             </GridItem>
 
             <GridItem>
-              <Text
-                mb="s16"
-                color="neutralColorLight.Gray-80"
-                fontSize="s3"
-                fontWeight="SemiBold"
-              >
+              <Text mb="s16" color="neutralColorLight.Gray-80" fontSize="s3" fontWeight="SemiBold">
                 {t['kymCoopUnionEcoCurrent']}
               </Text>
             </GridItem>
 
             <GridItem>
-              <Text
-                mb="s16"
-                color="neutralColorLight.Gray-80"
-                fontSize="s3"
-                fontWeight="SemiBold"
-              >
+              <Text mb="s16" color="neutralColorLight.Gray-80" fontSize="s3" fontWeight="SemiBold">
                 {t['kymCoopUnionEcoTarget']}
               </Text>
             </GridItem>
@@ -135,19 +118,9 @@ export const KymAssestsAndtarget = ({
 
           <Divider />
 
-          <Grid
-            alignItems="center"
-            px="s8"
-            py="s12"
-            templateColumns="repeat(3,1fr)"
-            gap="s40"
-          >
+          <Grid alignItems="center" px="s8" py="s12" templateColumns="repeat(3,1fr)" gap="s40">
             <GridItem>
-              <Text
-                color="neutralColorLight.Gray-80"
-                fontSize="s3"
-                fontWeight="Regular"
-              >
+              <Text color="neutralColorLight.Gray-80" fontSize="s3" fontWeight="Regular">
                 {t['kymCoopUnionEcoCashAndCashEquivalents']}
               </Text>
             </GridItem>
@@ -157,7 +130,6 @@ export const KymAssestsAndtarget = ({
                 type="number"
                 min={0}
                 name="cashAndCashEquivalentCurrent"
-                __placeholder="0.00"
               />
             </GridItem>
             <GridItem>
@@ -166,168 +138,69 @@ export const KymAssestsAndtarget = ({
                 type="number"
                 min={0}
                 name="cashAndCashEquivalentTarget"
-                __placeholder="0.00"
               />
             </GridItem>
           </Grid>
           <Divider />
-          <Grid
-            alignItems="center"
-            px="s8"
-            py="s12"
-            templateColumns="repeat(3,1fr)"
-            gap="s40"
-          >
+          <Grid alignItems="center" px="s8" py="s12" templateColumns="repeat(3,1fr)" gap="s40">
             <GridItem>
-              <Text
-                color="neutralColorLight.Gray-80"
-                fontSize="s3"
-                fontWeight="Regular"
-              >
+              <Text color="neutralColorLight.Gray-80" fontSize="s3" fontWeight="Regular">
                 {t['kymCoopUnionEcoBank']}
               </Text>
             </GridItem>
             <GridItem>
-              <FormInput
-                textAlign="right"
-                type="number"
-                min={0}
-                name="bankCurrent"
-                __placeholder="0.00"
-              />
+              <FormInput textAlign="right" type="number" min={0} name="bankCurrent" />
             </GridItem>
             <GridItem>
-              <FormInput
-                textAlign="right"
-                type="number"
-                min={0}
-                name="bankTarget"
-                __placeholder="0.00"
-              />
+              <FormInput textAlign="right" type="number" min={0} name="bankTarget" />
             </GridItem>
           </Grid>
           <Divider />
-          <Grid
-            alignItems="center"
-            px="s8"
-            py="s12"
-            templateColumns="repeat(3,1fr)"
-            gap="s40"
-          >
+          <Grid alignItems="center" px="s8" py="s12" templateColumns="repeat(3,1fr)" gap="s40">
             <GridItem>
-              <Text
-                color="neutralColorLight.Gray-80"
-                fontSize="s3"
-                fontWeight="Regular"
-              >
+              <Text color="neutralColorLight.Gray-80" fontSize="s3" fontWeight="Regular">
                 {t['kymCoopUnionEcoInvestments']}
               </Text>
             </GridItem>
             <GridItem>
-              <FormInput
-                textAlign="right"
-                type="number"
-                min={0}
-                name="investmentsCurrent"
-                __placeholder="0.00"
-              />
+              <FormInput textAlign="right" type="number" min={0} name="investmentsCurrent" />
             </GridItem>
             <GridItem>
-              <FormInput
-                textAlign="right"
-                type="number"
-                min={0}
-                name="investmentsTarget"
-                __placeholder="0.00"
-              />
+              <FormInput textAlign="right" type="number" min={0} name="investmentsTarget" />
             </GridItem>
           </Grid>
           <Divider />
-          <Grid
-            alignItems="center"
-            px="s8"
-            py="s12"
-            templateColumns="repeat(3,1fr)"
-            gap="s40"
-          >
+          <Grid alignItems="center" px="s8" py="s12" templateColumns="repeat(3,1fr)" gap="s40">
             <GridItem>
-              <Text
-                color="neutralColorLight.Gray-80"
-                fontSize="s3"
-                fontWeight="Regular"
-              >
+              <Text color="neutralColorLight.Gray-80" fontSize="s3" fontWeight="Regular">
                 {t['kymCoopUnionEcoLoanInv']}
               </Text>
             </GridItem>
             <GridItem>
-              <FormInput
-                textAlign="right"
-                type="number"
-                min={0}
-                name="loanCurrent"
-                __placeholder="0.00"
-              />
+              <FormInput textAlign="right" type="number" min={0} name="loanCurrent" />
             </GridItem>
             <GridItem>
-              <FormInput
-                textAlign="right"
-                type="number"
-                min={0}
-                name="loanTarget"
-                __placeholder="0.00"
-              />
+              <FormInput textAlign="right" type="number" min={0} name="loanTarget" />
             </GridItem>
           </Grid>
           <Divider />
-          <Grid
-            alignItems="center"
-            px="s8"
-            py="s12"
-            templateColumns="repeat(3,1fr)"
-            gap="s40"
-          >
+          <Grid alignItems="center" px="s8" py="s12" templateColumns="repeat(3,1fr)" gap="s40">
             <GridItem>
-              <Text
-                color="neutralColorLight.Gray-80"
-                fontSize="s3"
-                fontWeight="Regular"
-              >
+              <Text color="neutralColorLight.Gray-80" fontSize="s3" fontWeight="Regular">
                 {t['kymCoopUnionEcoNonCurrentAssets']}
               </Text>
             </GridItem>
             <GridItem>
-              <FormInput
-                textAlign="right"
-                type="number"
-                min={0}
-                name="nonCurrentAssetsCurrent"
-                __placeholder="0.00"
-              />
+              <FormInput textAlign="right" type="number" min={0} name="nonCurrentAssetsCurrent" />
             </GridItem>
             <GridItem>
-              <FormInput
-                textAlign="right"
-                type="number"
-                min={0}
-                name="nonCurrentAssetsTarget"
-                __placeholder="0.00"
-              />
+              <FormInput textAlign="right" type="number" min={0} name="nonCurrentAssetsTarget" />
             </GridItem>
           </Grid>
           <Divider />
-          <Grid
-            alignItems="center"
-            px="s8"
-            py="s12"
-            templateColumns="repeat(3,1fr)"
-            gap="s40"
-          >
+          <Grid alignItems="center" px="s8" py="s12" templateColumns="repeat(3,1fr)" gap="s40">
             <GridItem>
-              <Text
-                color="neutralColorLight.Gray-80"
-                fontSize="s3"
-                fontWeight="Regular"
-              >
+              <Text color="neutralColorLight.Gray-80" fontSize="s3" fontWeight="Regular">
                 {t['kymCoopUnionEcootherNonCurrentAssets']}
               </Text>
             </GridItem>
@@ -337,7 +210,6 @@ export const KymAssestsAndtarget = ({
                 type="number"
                 min={0}
                 name="otherNonCurrentAssetsCurrent"
-                __placeholder="0.00"
               />
             </GridItem>
             <GridItem>
@@ -346,33 +218,21 @@ export const KymAssestsAndtarget = ({
                 type="number"
                 min={0}
                 name="otherNonCurrentAssetsTarget"
-                __placeholder="0.00"
               />
             </GridItem>
           </Grid>
           <Divider />
           {/* .................................last part ............................... */}
-          <Grid
-            alignItems="center"
-            px="s8"
-            py="s16"
-            templateColumns="repeat(3,1fr)"
-            gap="s40"
-          >
+          <Grid alignItems="center" px="s8" py="s16" templateColumns="repeat(3,1fr)" gap="s40">
             <GridItem>
-              <Text
-                mb="s16"
-                color="neutralColorLight.Gray-80"
-                fontSize="s3"
-                fontWeight="SemiBold"
-              >
+              <Text mb="s16" color="neutralColorLight.Gray-80" fontSize="s3" fontWeight="SemiBold">
                 {t['kymCoopUnionRepTotal']}
               </Text>
             </GridItem>
 
             <GridItem>
               <FormInput
-                isDisabled={true}
+                isDisabled
                 bg="neutralColorLight.Gray-20"
                 border="1px solid"
                 borderColor="disabled.disabled"
@@ -380,12 +240,11 @@ export const KymAssestsAndtarget = ({
                 type="text"
                 name="totalAssetsCurrent"
                 value={totalassestscurrent}
-                __placeholder={t['kymCoopUnionEcoTotalAssets']}
               />
             </GridItem>
             <GridItem>
               <FormInput
-                isDisabled={true}
+                isDisabled
                 bg="neutralColorLight.Gray-20"
                 border="1px solid"
                 borderColor="disabled.disabled"
@@ -393,7 +252,6 @@ export const KymAssestsAndtarget = ({
                 type="text"
                 name="totalAssetsTarget"
                 value={totalassestsTARGET}
-                __placeholder={t['kymCoopUnionEcoTotal']}
               />
             </GridItem>
           </Grid>
