fe0022729f00f8f42eadff281d7759d1556daacb
Suyash <suyashshakya.h0p3@gmail.com>
2023-01-02 16:20:19 +0545

    fix(index_reset): index reset

--- apps/myra/pages/settings/general/indexing/index.tsx
+++ apps/myra/pages/settings/general/indexing/index.tsx
@@ -1,18 +1,24 @@
-import { Box, Button, Text } from '@myra-ui';
+import { Box, Button, Text, toast } from '@myra-ui';
 
 import { useSearchIndexingMutation } from '@coop/cbs/data-access';
 import { SettingsGeneralLayout, SettingsLayout } from '@coop/cbs/settings/ui-layout';
 
 const Indexing = () => {
-  const { mutate } = useSearchIndexingMutation({});
+  const { mutateAsync, isLoading } = useSearchIndexingMutation({});
   const onClick = () => {
-    mutate({});
+    mutateAsync({}).then(() => {
+      toast({
+        id: 'indexing',
+        type: 'success',
+        message: 'Index reset successfully',
+      });
+    });
   };
   return (
     <Box display="flex" gap={5} flexDirection="column" p={5}>
       <Text>Indexing</Text>
-      <Button onClick={onClick} width={150}>
-        Fetch all index
+      <Button onClick={onClick} width={150} isLoading={isLoading}>
+        Reset Search index
       </Button>
     </Box>
   );
