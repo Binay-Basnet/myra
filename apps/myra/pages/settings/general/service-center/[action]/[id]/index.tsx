d6a1391812f9d7fa2c5f70bbfd30c6c6c4074378
Anup Shrestha <anup.stha012@gmail.com>
2023-01-04 13:58:37 +0545

    fix(routes): settings routes

--- apps/myra/pages/settings/general/service-center/[action]/[id]/index.tsx
+++
@@ -1,9 +0,0 @@
-import { CbsSettingsFeatureServiceCenterNew } from '@coop/cbs/settings/branches';
-import { SettingsLayout } from '@coop/cbs/settings/ui-layout';
-
-const Branches = () => <CbsSettingsFeatureServiceCenterNew />;
-
-export default Branches;
-Branches.getLayout = function getLayout(page) {
-  return <SettingsLayout>{page}</SettingsLayout>;
-};
