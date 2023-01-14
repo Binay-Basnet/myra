9a0282e346c4eda6d2ff66cc7c47f4580c577308
Anup Shrestha <anup.stha012@gmail.com>
2023-01-14 09:40:18 +0545

    feat(project): add schema path from local storage

--- libs/shared/utils/src/utilFunctions/getSchemaPath.ts
+++ libs/shared/utils/src/utilFunctions/getSchemaPath.ts
@@ -8,3 +8,14 @@ export const getSchemaPath = () => {
 
   return `${process.env['NX_SCHEMA_PATH']}/query`;
 };
+
+export const getLoginSchemaPath = () => {
+  const schemaPathFromLocalStorage =
+    typeof window !== 'undefined' ? window.localStorage.getItem('url') : null;
+
+  if (schemaPathFromLocalStorage) {
+    return schemaPathFromLocalStorage;
+  }
+
+  return `${process.env['NX_SCHEMA_PATH']}/erp/login`;
+};
