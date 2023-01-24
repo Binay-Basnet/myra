282d78b9de046e5ba33cc03f90180ef7699aff61
Suyash <suyashshakya.h0p3@gmail.com>
2023-01-24 11:44:11 +0545

    fix(hotfix): hotfix

--- libs/@myra/table/src/lib/shared-table.tsx
+++ libs/@myra/table/src/lib/shared-table.tsx
@@ -218,7 +218,7 @@ const TableWithoutRef = <T extends Record<string, unknown>>(
               (data?.length === 0 && (
                 <Box
                   // position="absolute"
-                  width="80vw"
+                  width="70vw"
                   as="tr"
                   height="100vh"
                   display="flex"
