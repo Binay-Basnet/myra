6b22cb84213edcf05ebecb5cf68ff4365f5ccedd
Ajay Prajapati <ajayprazz@gmail.com>
2023-01-25 18:47:25 +0545

    feat(transaction): update sidebar in account transfer detail page

--- libs/cbs/transactions/feature-detail-page/src/component/SideBar.tsx
+++ libs/cbs/transactions/feature-detail-page/src/component/SideBar.tsx
@@ -2,6 +2,8 @@ import { useRouter } from 'next/router';
 
 import { DetailPageTabs } from '@myra-ui';
 
+import { Member } from '@coop/cbs/data-access';
+
 import { MemberInfo } from './MemberInfo';
 import { Summary } from './Summary';
 
@@ -17,14 +19,18 @@ type SidebarProps = {
     amount?: string | undefined | null;
     method?: string | undefined | null;
     sourceAccount?: string | undefined | null;
+    sourceAccountId?: string | null | undefined;
     destinationName?: string | undefined | null;
     destinationAccount?: string | undefined | null;
+    destinationAccountId?: string | null | undefined;
     recipientMember?: string | undefined | null;
     loanAccountName?: string | undefined | null;
     loanSubtype?: string | undefined | null;
     loanAccId?: string | undefined | null;
     repaymentDate?: Record<'local' | 'en' | 'np', string> | undefined | null;
     interestRate?: string | undefined | null;
+    senderMember?: Member | null | undefined;
+    reciepentMemberDetails?: Member | null | undefined;
   };
   detailPage: 'deposit' | 'withdraw' | 'accountTransfer' | 'agentTransaction' | 'loanRepayment';
 };
@@ -45,9 +51,13 @@ export const SideBar = ({ summary, detailPage }: SidebarProps) => {
             name={summary?.name}
             profilePic={summary?.profilePic ?? ''}
             sourceAccount={summary?.sourceAccount ?? ''}
+            sourceAccountId={summary?.sourceAccountId}
             destinationName={summary?.destinationName ?? ' '}
+            destinationAccountId={summary?.destinationAccountId}
             destinationAccount={summary?.destinationAccount ?? ''}
             recipientMemberId={summary?.recipientMember ?? ''}
+            senderMember={summary?.senderMember}
+            reciepentMember={summary?.reciepentMemberDetails}
           />
         </>
       )}
