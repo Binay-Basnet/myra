d2677114c728f608dd0c844635b9ad2344cec293
Anup Shrestha <anup.stha012@gmail.com>
2023-01-31 11:19:55 +0545

    fix(auth): build fix

--- libs/cbs/data-access/src/generated/axiosHelper.ts
+++ libs/cbs/data-access/src/generated/axiosHelper.ts
@@ -132,16 +132,13 @@ interface RefreshTokenResponse {
 export const useAxios =
   <TData, TVariables>(
     query: string
-  ): ((
-    variables?: TVariables,
-    config?: AxiosRequestConfig<TData>
-  ) => Promise<TData | { error: { message: string }[] }>) =>
+  ): ((variables?: TVariables, config?: AxiosRequestConfig<TData>) => Promise<TData>) =>
   async (variables?: TVariables, config?: AxiosRequestConfig<TData>) =>
     privateAgent
       .post<{ data: TData; errors: { message: string }[] }>('/query', { query, variables }, config)
       .then((res) => {
         if (!res.data.data || res.data.errors) {
-          return { error: res.data.errors };
+          return { error: res.data.errors } as TData;
         }
 
         return res.data.data;
