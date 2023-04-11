7a9da10017028308050df65b28c9abc1e42d603a
Anup Shrestha <anup.stha012@gmail.com>
2023-04-11 11:25:25 +0545

    feat(layout): add layout and routes for committee

--- libs/shared/utils/src/utilFunctions/getDatabaseSlug.ts
+++ libs/shared/utils/src/utilFunctions/getDatabaseSlug.ts
@@ -1,5 +1,5 @@
 export const getDatabaseSlug = () => {
-  if (typeof window === 'undefined') return 'myra-prod';
+  if (typeof window === 'undefined') return process.env['NX_SLUG'] || 'myra-prod';
 
   const slug = process.env['NX_SLUG'] || window.location.host.split('.')[0];
   if (slug.includes('localhost')) return process.env['NX_SLUG'] || 'myra-prod';
