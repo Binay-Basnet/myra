12eaa5e001d96c1242bfd6e0e77c63b0a705f606
Ajay Prajapati <ajayprazz@gmail.com>
2023-05-03 13:10:21 +0545

    feat(cbs): implement form layout in add deposit

--- apps/myra/pages/cbs/savings/account-close/add/index.tsx
+++ apps/myra/pages/cbs/savings/account-close/add/index.tsx
@@ -1,16 +1,12 @@
 import { ReactElement } from 'react';
 
-import { MainLayout, Scrollable } from '@myra-ui';
+import { MainLayout } from '@myra-ui';
 
 import { CbsAccountClose } from '@coop/cbs/accounts/account-form';
 
 const AccountCloseAdd = () => <CbsAccountClose />;
 
 AccountCloseAdd.getLayout = function getLayout(page: ReactElement) {
-  return (
-    <MainLayout>
-      <Scrollable>{page}</Scrollable>
-    </MainLayout>
-  );
+  return <MainLayout>{page}</MainLayout>;
 };
 export default AccountCloseAdd;
