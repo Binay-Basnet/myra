d3a7a656c8f9814261682702b0f3e3010d4c6f7a
Suyash <48352428+SuyashShakya@users.noreply.github.com>
2023-05-12 12:52:14 +0545

    feat(dhadda-migration): dhadda migration auth (#1315)
    * feat(dhadda-migration): dhadda migration auth
    
    * fix(dhadda): dhadda migration

--- apps/dhadda-migration/pages/_app.tsx
+++ apps/dhadda-migration/pages/_app.tsx
@@ -1,12 +1,24 @@
+import { ReactElement, ReactNode } from 'react';
+import { Provider } from 'react-redux';
+import type { NextPage } from 'next';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
+import { store, useInit } from '@dhadda-migration/data-access';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import { Toaster } from '@myra-ui';
+import { Box, Loader, Toaster } from '@myra-ui';
 import { theme } from '@myra-ui/theme';
 
 import './styles.css';
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
 
 const fiveMinutesInMs = 5 * 60 * 1000;
 
@@ -24,18 +36,36 @@ const queryClient = new QueryClient({
   },
 });
 
-const CustomApp = ({ Component, pageProps }: AppProps) => (
-  <ChakraProvider theme={theme}>
-    <QueryClientProvider client={queryClient}>
+const MainApp = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const { isLoading } = useInit();
+
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return (
+    <>
       <Head>
         <title>Dhadda-migration!</title>
       </Head>
       <Toaster />
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </QueryClientProvider>
-  </ChakraProvider>
+      {isLoading ? (
+        <Box h="100vh" bg="white" display="flex" alignItems="center" justifyContent="center">
+          <Loader height={300} />
+        </Box>
+      ) : (
+        <main className="app">{getLayout(<Component {...pageProps} />)}</main>
+      )}
+    </>
+  );
+};
+
+const CustomApp = (props: AppPropsWithLayout) => (
+  <Provider store={store}>
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <MainApp {...props} />
+      </QueryClientProvider>
+    </ChakraProvider>
+  </Provider>
 );
 
 export default CustomApp;
---
+++ libs/dhadda-migration/data-access/src/redux/useInit.ts
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import {
+  logout,
+  useAppDispatch,
+  // useGetMeQuery,
+  useRefreshToken,
+} from '@dhadda-migration/data-access';
+
+import { getSchemaPath, useReplace } from '@coop/shared/utils';
+
+const url = getSchemaPath();
+
+export const useInit = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  // const [triggerQuery, setTriggerQuery] = React.useState(false);
+  const dispatch = useAppDispatch();
+  const replace = useReplace();
+
+  // const getMe = useGetMeQuery(
+  //   {},
+  //   {
+  //     enabled: triggerQuery,
+  //   }
+  // );
+
+  const refreshToken = useRefreshToken(url);
+
+  // const hasDataReturned = getMe?.data?.neosys?.auth;
+  // const hasData = getMe?.data?.neosys?.auth?.me?.data;
+  // const userData = getMe?.data?.neosys?.auth?.me?.data;
+
+  useEffect(() => {
+    refreshToken()
+      .then((res) => {
+        if (res) {
+          setIsLoading(false);
+          // setTriggerQuery(true);
+        }
+      })
+      .catch(() => {
+        dispatch(logout());
+        replace('/login').then(() => setIsLoading(false));
+      });
+  }, [dispatch, refreshToken, replace]);
+
+  // useEffect(() => {
+  //   if (hasDataReturned) {
+  //     if (userData) {
+  //       dispatch(authenticate({ user: userData }));
+  //       setIsLoading(false);
+  //     } else {
+  //       dispatch(logout());
+  //       replace('/login').then(() => setIsLoading(false));
+  //     }
+  //   }
+  // }, [dispatch, hasDataReturned, hasData, userData, replace]);
+
+  return { isLoading };
+};
