c0af2f231726bb59802ee1dd024f7d7660be1c55
Ajay Prajapati <ajayprazz@gmail.com>
2023-06-14 13:16:30 +0545

    fix(cbs): fix cash in transit denomination

--- libs/cbs/transfer/src/lib/AddCashTransitTransfer.tsx
+++ libs/cbs/transfer/src/lib/AddCashTransitTransfer.tsx
@@ -49,6 +49,19 @@ export const AddCashTransitTransfer = () => {
       transferMode: CashTransferMode.Collected,
       senderTeller: [user?.firstName?.local, user?.lastName?.local].join(' '),
       senderServiceCentre: branchData?.name,
+      denomination: [
+        { value: '1000', quantity: 0, amount: '0' },
+        { value: '500', quantity: 0, amount: '0' },
+        { value: '100', quantity: 0, amount: '0' },
+        { value: '50', quantity: 0, amount: '0' },
+        { value: '25', quantity: 0, amount: '0' },
+        { value: '20', quantity: 0, amount: '0' },
+        { value: '10', quantity: 0, amount: '0' },
+        { value: '5', quantity: 0, amount: '0' },
+        { value: '2', quantity: 0, amount: '0' },
+        { value: '1', quantity: 0, amount: '0' },
+        { value: 'PAISA', quantity: 0, amount: '0' },
+      ],
     },
   });
 
