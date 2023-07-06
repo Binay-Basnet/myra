ad5ea0e4ee269ded95565fd07ca2fca16f9e580e
sameergautam01 <sameer.gautam9860@gmail.com>
2023-07-06 09:57:17 +0545

    feat(payroll): implement custom date range selector in payroll entry

--- libs/hr-module/feature-payroll/src/components/payroll-entry/BasicDetails.tsx
+++ libs/hr-module/feature-payroll/src/components/payroll-entry/BasicDetails.tsx
@@ -1,11 +1,14 @@
-import { FormSection } from '@myra-ui';
+import { FormSection, GridItem } from '@myra-ui';
 
+import { ReportCustomDateRange } from '@coop/cbs/reports/components';
 import { FormDatePicker } from '@coop/shared/form';
 
 export const PayrollEntryBasicDetails = () => (
   <FormSection>
-    <FormDatePicker name="payrollPeriodStart" label="Payroll Period Start Date" />
-    <FormDatePicker name="payrollPeriodEnd" label="Payroll Period End Date" />
+    <GridItem colSpan={2}>
+      <ReportCustomDateRange name="payrollPeriod" label="Payroll Period " />
+    </GridItem>
+    {/* <FormDatePicker name="payrollPeriodEnd" label="Payroll Period End Date" /> */}
     <FormDatePicker name="payDay" label="Pay Day" />
   </FormSection>
 );
