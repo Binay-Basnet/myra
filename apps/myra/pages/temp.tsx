242d4484ef72c93871059c49fe44ced4569c8599
Anup Shrestha <anup.stha012@gmail.com>
2023-07-07 13:43:00 +0545

    fix(date): date picker not selectable in coop and coop union

--- apps/myra/pages/temp.tsx
+++
@@ -1,7 +0,0 @@
-import { DatePicker } from '@myra-ui/date-picker';
-
-const JobApplicationAdd = () => (
-  <DatePicker calendarType="BS" minDate={new Date('2023-07-04')} maxDate={new Date('2023-07-05')} />
-);
-
-export default JobApplicationAdd;
