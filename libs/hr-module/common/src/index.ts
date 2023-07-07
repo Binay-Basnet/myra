07e1ce1b49e2973408943cd04a53b760ee44b5bb
Suyash <suyashshakya.h0p3@gmail.com>
2023-07-07 14:25:24 +0545

    fix(hr): hr select options fix

--- libs/hr-module/common/src/index.ts
+++ libs/hr-module/common/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/useGetDepartmentOptions';
 export * from './lib/useGetDesignationOptions';
 export * from './lib/useGetEmployeeOptions';
 export * from './lib/useGetJobApplicantOptions';
+export * from './lib/useGetJobOpeningOptions';
+export * from './lib/useGetStaffPlanningOptions';
