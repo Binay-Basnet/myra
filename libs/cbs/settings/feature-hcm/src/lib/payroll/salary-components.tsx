211ffb487d350b63509603d0ff008cde9c42469e
Suyash <suyashshakya.h0p3@gmail.com>
2023-07-13 14:12:44 +0545

    feat(hr_settings_payroll): deduction component

--- libs/cbs/settings/feature-hcm/src/lib/payroll/salary-components.tsx
+++ libs/cbs/settings/feature-hcm/src/lib/payroll/salary-components.tsx
@@ -1,5 +1,6 @@
 import { Box, Divider, Text } from '@myra-ui';
 
+import DeductionComponentTable from '../../components/payroll/DeductionComponentTable';
 import EarningComponentTable from '../../components/payroll/EarningComponentTable';
 
 export const SalaryComponents = () => (
@@ -13,6 +14,7 @@ export const SalaryComponents = () => (
     <Box display="flex">
       <Box display="flex" flexDir="column" flex={1} gap="s16" mr={200}>
         <EarningComponentTable />
+        <DeductionComponentTable />
       </Box>
       <Box position="fixed" right={0}>
         <Box display="flex" flexDir="column" minW="200" px="s16" gap="s16">
