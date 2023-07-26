5f0b79d9b5739e68fd3db2b139d379b31b6fcda8
sameergautam01 <sameer.gautam9860@gmail.com>
2023-07-26 11:54:28 +0545

    fix(report): minor changes

--- libs/cbs/reports/feature-reports/src/lib/members/MemberTransferReport.tsx
+++ libs/cbs/reports/feature-reports/src/lib/members/MemberTransferReport.tsx
@@ -17,7 +17,7 @@ import { ReportDateRange } from '@coop/cbs/reports/components';
 import { Report as ReportEnum } from '@coop/cbs/reports/list';
 import { localizedDate, RouteToDetailsPage } from '@coop/cbs/utils';
 import { FormBranchSelect, FormSelect } from '@coop/shared/form';
-import { amountConverter, debitCreditConverter, quantityConverter } from '@coop/shared/utils';
+import { amountConverter, debitCreditConverter } from '@coop/shared/utils';
 
 type LoanGuranteeData = Partial<{
   memberId: string;
@@ -179,7 +179,7 @@ export const MemberTransferReport = () => {
                     header: 'Share Balance',
                     accessorKey: 'shareBalance',
                     cell: (props) =>
-                      quantityConverter(props?.row?.original?.shareBalance as string | '0'),
+                      amountConverter(props?.row?.original?.shareBalance as string | '0'),
                   },
                 ],
               },
