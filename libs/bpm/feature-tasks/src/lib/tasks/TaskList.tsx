8b56b988f5336a637af1ff7463efc711b8cb1aa3
suyash shakya <suyashshakya.h0p3@gmail.com>
2023-08-09 08:50:14 +0545

    feat(bpm_task): task assign

--- libs/bpm/feature-tasks/src/lib/tasks/TaskList.tsx
+++ libs/bpm/feature-tasks/src/lib/tasks/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
 import { useRouter } from 'next/router';
 import { Portal } from '@chakra-ui/react';
@@ -20,14 +20,44 @@ import { LeaveStatusEnum, useGetTaskListQuery } from '@coop/cbs/data-access';
 import { ROUTES } from '@coop/cbs/utils';
 import { getPaginationQuery } from '@coop/shared/utils';
 
+import AssignModal from './components/AssignModal';
 import PopoverItem from './components/PopoverItem';
+
+const popOverItemData = [
+  {
+    itemColor: 'red.500',
+    itemText: 'Assigned',
+  },
+  {
+    itemColor: 'yellow.500',
+    itemText: 'Pending',
+  },
+  {
+    itemColor: 'blue.500',
+    itemText: 'Started',
+  },
+  {
+    itemColor: 'red.500',
+    itemText: 'Completed',
+  },
+];
 
 export const TaskList = () => {
   const router = useRouter();
+  const [selectedTaskId, setSelectedTaskId] = useState('');
+  const [isAssignModalOpen, setIsAssignModalOpen] = useState(false);
 
-  const { data, isFetching } = useGetTaskListQuery({
+  const { data, isFetching, refetch } = useGetTaskListQuery({
     pagination: getPaginationQuery(),
   });
+
+  const handleClearTaskId = () => {
+    setSelectedTaskId('');
+  };
+
+  const handleCloseAssignModal = () => {
+    setIsAssignModalOpen(false);
+  };
 
   const rowData = useMemo(() => data?.bpm?.task?.listTask?.edges ?? [], [data]);
 
@@ -57,55 +87,35 @@ export const TaskList = () => {
         header: 'Status',
         accessorFn: (row) => row?.node?.status,
 
-        cell: (props) => {
-          const popOverItemData = [
-            {
-              itemColor: 'red.500',
-              itemText: 'Assigned',
-            },
-            {
-              itemColor: 'yellow.500',
-              itemText: 'Pending',
-            },
-            {
-              itemColor: 'blue.500',
-              itemText: 'Started',
-            },
-            {
-              itemColor: 'red.500',
-              itemText: 'Completed',
-            },
-          ];
-          return (
-            <Box display="flex" alignItems="center" gap="s8">
-              <ApprovalStatusItem status={props?.row?.original?.node?.status as LeaveStatusEnum} />
+        cell: (props) => (
+          <Box display="flex" alignItems="center" gap="s8">
+            <ApprovalStatusItem status={props?.row?.original?.node?.status as LeaveStatusEnum} />
 
-              <Popover isLazy placement="bottom-start" colorScheme="primary">
-                {({ onClose }) => (
-                  <>
-                    <PopoverTrigger>
-                      <Box as="button" display="flex" alignItems="center">
-                        <Icon size="sm" as={IoIosArrowDown} />
-                      </Box>
-                    </PopoverTrigger>
-                    <Portal>
-                      <PopoverContent w="100%" boxShadow="E2" border="none" borderRadius="br2">
-                        {popOverItemData?.map((item) => (
-                          <PopoverItem
-                            itemColor={item?.itemColor}
-                            itemText={item?.itemText}
-                            taskId={props?.cell?.row?.original?.node?.id}
-                            onClose={onClose}
-                          />
-                        ))}
-                      </PopoverContent>
-                    </Portal>
-                  </>
-                )}
-              </Popover>
-            </Box>
-          );
-        },
+            <Popover isLazy placement="bottom-start" colorScheme="primary">
+              {({ onClose }) => (
+                <>
+                  <PopoverTrigger>
+                    <Box as="button" display="flex" alignItems="center">
+                      <Icon size="sm" as={IoIosArrowDown} />
+                    </Box>
+                  </PopoverTrigger>
+                  <Portal>
+                    <PopoverContent w="100%" boxShadow="E2" border="none" borderRadius="br2">
+                      {popOverItemData?.map((item) => (
+                        <PopoverItem
+                          itemColor={item?.itemColor}
+                          itemText={item?.itemText}
+                          taskId={props?.cell?.row?.original?.node?.id}
+                          onClose={onClose}
+                        />
+                      ))}
+                    </PopoverContent>
+                  </Portal>
+                </>
+              )}
+            </Popover>
+          </Box>
+        ),
       },
       {
         id: '_actions',
@@ -119,6 +129,13 @@ export const TaskList = () => {
                   title: 'Edit',
                   onClick: (row) => {
                     router.push(`${ROUTES?.BPM_TASKS_ADD}?id=${row?.id}`);
+                  },
+                },
+                {
+                  title: 'Assign Task',
+                  onClick: (row) => {
+                    setSelectedTaskId(row?.id);
+                    setIsAssignModalOpen(true);
                   },
                 },
               ]}
@@ -143,6 +160,13 @@ export const TaskList = () => {
           total: data?.bpm?.task?.listTask?.totalCount as number,
           pageInfo: data?.bpm?.task?.listTask?.pageInfo,
         }}
+      />
+      <AssignModal
+        isAssignModalOpen={isAssignModalOpen}
+        selectedTaskId={selectedTaskId}
+        handleClearTaskId={handleClearTaskId}
+        handleCloseAssignModal={handleCloseAssignModal}
+        refetch={refetch}
       />
     </>
   );
