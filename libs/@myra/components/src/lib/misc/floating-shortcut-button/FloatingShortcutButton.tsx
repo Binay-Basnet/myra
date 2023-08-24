a8936ad94c85aca3477ba8fbe12c504365cde4c1
Suyash <suyashshakya.h0p3@gmail.com>
2023-08-24 13:28:39 +0545

    feat(new): release note

--- libs/@myra/components/src/lib/misc/floating-shortcut-button/FloatingShortcutButton.tsx
+++ libs/@myra/components/src/lib/misc/floating-shortcut-button/FloatingShortcutButton.tsx
@@ -83,7 +83,7 @@ const WhatsNewModal = (props: WhatsNewModalProps) => {
     >
       <Box p={3} w="100%" display="flex" flexDirection="column" gap={5}>
         <Box display="flex" justifyContent="space-between">
-          <Text fontSize="r2">Version 1.0.88</Text>
+          <Text fontSize="r2">Version 1.0.89</Text>
           <Text fontSize="s3">August 24, 2023</Text>
         </Box>
         {!isEmpty(whatsNewData.features.data) && (
