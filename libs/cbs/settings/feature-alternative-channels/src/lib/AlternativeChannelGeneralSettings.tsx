b5f5c6f495ca6b2d41a87b10910ea1b9da6ce99b
Ajay Prajapati <ajayprazz@gmail.com>
2023-08-28 12:14:25 +0545

    fix(settings): fix utility payment general settings update

--- libs/cbs/settings/feature-alternative-channels/src/lib/AlternativeChannelGeneralSettings.tsx
+++ libs/cbs/settings/feature-alternative-channels/src/lib/AlternativeChannelGeneralSettings.tsx
@@ -126,21 +126,27 @@ export const AlternativeChannelGeneralSettings = () => {
               value: [
                 {
                   coaHead:
-                    values && 'value' in values[UtilityLedgerType.Utility]
+                    values &&
+                    typeof values[UtilityLedgerType.Utility] === 'object' &&
+                    'value' in values[UtilityLedgerType.Utility]
                       ? values?.[UtilityLedgerType.Utility]?.value
                       : values?.[UtilityLedgerType.Utility],
                   ledgerType: UtilityLedgerType.Utility,
                 },
                 {
                   coaHead:
-                    values && 'value' in values[UtilityLedgerType.CashBack]
+                    values &&
+                    typeof values[UtilityLedgerType.CashBack] === 'object' &&
+                    'value' in values[UtilityLedgerType.CashBack]
                       ? values?.[UtilityLedgerType.CashBack]?.value
                       : values?.[UtilityLedgerType.CashBack],
                   ledgerType: UtilityLedgerType.CashBack,
                 },
                 {
                   coaHead:
-                    values && 'value' in values[UtilityLedgerType.ServiceCharge]
+                    values &&
+                    typeof values[UtilityLedgerType.ServiceCharge] === 'object' &&
+                    'value' in values[UtilityLedgerType.ServiceCharge]
                       ? values?.[UtilityLedgerType.ServiceCharge]?.value
                       : values?.[UtilityLedgerType.ServiceCharge],
                   ledgerType: UtilityLedgerType.ServiceCharge,
