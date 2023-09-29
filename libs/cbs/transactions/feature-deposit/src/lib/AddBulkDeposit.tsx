aee9dd6a5353fd90c28d7baf563aee0cfc3bbb99
sameergautam01 <sameer.gautam9860@gmail.com>
2023-09-29 11:27:46 +0545

    fix(bulk): add new form layout to bulk depsoit

--- libs/cbs/transactions/feature-deposit/src/lib/AddBulkDeposit.tsx
+++ libs/cbs/transactions/feature-deposit/src/lib/AddBulkDeposit.tsx
@@ -1,19 +1,9 @@
 import { useMemo, useState } from 'react';
-import { FormProvider, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { useRouter } from 'next/router';
-import { useQueryClient } from '@tanstack/react-query';
 import omit from 'lodash/omit';
 
-import {
-  asyncToast,
-  Box,
-  Button,
-  Container,
-  FormFooter,
-  FormHeader,
-  MemberCard,
-  Text,
-} from '@myra-ui';
+import { Box, Button, MemberCard, Text } from '@myra-ui';
 
 import {
   BulkDepositInput,
@@ -21,10 +11,9 @@ import {
   DepositedBy,
   DepositPaymentType,
   useGetIndividualMemberDetails,
-  useSetBulkDepositDataMutation,
 } from '@coop/cbs/data-access';
 import { ROUTES } from '@coop/cbs/utils';
-import { FormMemberSelect } from '@coop/shared/form';
+import { FormLayout, FormMemberSelect } from '@coop/shared/form';
 
 import { BulkDepositAccountsSummary, BulkDepositAccountsTable, Payment } from '../components';
 
@@ -60,7 +49,7 @@ const cashOptions: Record<string, string> = {
 export const AddBulkDeposit = () => {
   const router = useRouter();
 
-  const queryClient = useQueryClient();
+  // const queryClient = useQueryClient();
 
   const methods = useForm<CustomBulkDepositInput>({
     defaultValues: {
@@ -79,7 +68,7 @@ export const AddBulkDeposit = () => {
 
   const [mode, setMode] = useState<number>(0); // 0: form 1: payment
 
-  const { mutateAsync } = useSetBulkDepositDataMutation();
+  // const { mutateAsync } = useSetBulkDepositDataMutation();
 
   const disableDenomination = watch('cash.disableDenomination');
 
@@ -139,18 +128,19 @@ export const AddBulkDeposit = () => {
       filteredValues = omit({ ...filteredValues }, ['bankVoucher', 'cash']);
     }
 
-    asyncToast({
-      id: 'add-bulk-deposit-transaction',
-      msgs: {
-        loading: 'Adding bulk deposit',
-        success: 'Bulk deposit added',
-      },
-      promise: mutateAsync({ data: filteredValues as BulkDepositInput }),
-      onSuccess: () => {
-        queryClient.invalidateQueries(['getDepositListData']);
-        router.push(ROUTES.CBS_TRANS_DEPOSIT_LIST);
-      },
-    });
+    // asyncToast({
+    //   id: 'add-bulk-deposit-transaction',
+    //   msgs: {
+    //     loading: 'Adding bulk deposit',
+    //     success: 'Bulk deposit added',
+    //   },
+    //   promise: mutateAsync({ data: filteredValues as BulkDepositInput }),
+    //   onSuccess: () => {
+    //     queryClient.invalidateQueries(['getDepositListData']);
+    //     router.push(ROUTES.CBS_TRANS_DEPOSIT_LIST);
+    //   },
+    // });
+    return filteredValues as BulkDepositInput;
   };
 
   const accounts = watch('accounts');
@@ -188,95 +178,80 @@ export const AddBulkDeposit = () => {
   const paymentModes = watch('payment_type');
 
   return (
-    <>
-      <Container minW="container.xl" height="fit-content">
-        <Box position="sticky" top="0" bg="gray.100" width="100%" zIndex="10">
-          <FormHeader
-            title="New Bulk Deposit"
-            closeLink={ROUTES.CBS_TRANS_DEPOSIT_LIST}
-            buttonLabel="Add Deposit"
-            buttonHandler={() => router.push(ROUTES.CBS_TRANS_DEPOSIT_ADD)}
-          />
-        </Box>
+    <FormLayout methods={methods}>
+      <FormLayout.Header
+        title="New Bulk Deposit"
+        closeLink={ROUTES.CBS_TRANS_DEPOSIT_LIST}
+        buttonLabel="Add Deposit"
+        buttonHandler={() => router.push(ROUTES.CBS_TRANS_DEPOSIT_ADD)}
+      />
+      <FormLayout.Content>
+        <FormLayout.Form>
+          <Box display={mode === 0 ? 'flex' : 'none'} minH="calc(100vh - 170px)">
+            <Box p="s16" pb="100px" width="100%" display="flex" flexDirection="column" gap="s24">
+              <FormMemberSelect name="memberId" label="Member" />
+              {memberId && (
+                <MemberCard
+                  isInline
+                  memberDetails={{
+                    name: memberDetailData?.name,
+                    avatar: memberDetailData?.profilePicUrl ?? '',
+                    code: memberDetailData?.code,
+                    memberID: memberDetailData?.id,
+                    gender: memberDetailData?.gender,
+                    age: memberDetailData?.age,
+                    maritalStatus: memberDetailData?.maritalStatus as string,
+                    dateJoined: memberDetailData?.dateJoined,
+                    // branch: 'Basantapur',
+                    phoneNo: memberDetailData?.contact,
+                    email: memberDetailData?.email,
+                    address: memberDetailData?.address,
+                  }}
+                  // notice="KYM needs to be updated"
+                  signaturePath={memberSignatureUrl as string}
+                  citizenshipPath={memberCitizenshipUrl as string}
+                  cardBg="neutralColorLight.Gray-10"
+                />
+              )}
+              {memberId && <BulkDepositAccountsTable memberId={memberId} />}
 
-        <Box bg="white">
-          <FormProvider {...methods}>
-            <form>
-              <Box display={mode === 0 ? 'flex' : 'none'} minH="calc(100vh - 170px)">
-                <Box
-                  p="s16"
-                  pb="100px"
-                  width="100%"
-                  display="flex"
-                  flexDirection="column"
-                  gap="s24"
-                >
-                  <FormMemberSelect name="memberId" label="Member" />
+              {accounts?.length && <BulkDepositAccountsSummary memberId={memberId} />}
+            </Box>
+          </Box>
 
-                  {memberId && (
-                    <MemberCard
-                      isInline
-                      memberDetails={{
-                        name: memberDetailData?.name,
-                        avatar: memberDetailData?.profilePicUrl ?? '',
-                        code: memberDetailData?.code,
-                        memberID: memberDetailData?.id,
-                        gender: memberDetailData?.gender,
-                        age: memberDetailData?.age,
-                        maritalStatus: memberDetailData?.maritalStatus as string,
-                        dateJoined: memberDetailData?.dateJoined,
-                        // branch: 'Basantapur',
-                        phoneNo: memberDetailData?.contact,
-                        email: memberDetailData?.email,
-                        address: memberDetailData?.address,
-                      }}
-                      // notice="KYM needs to be updated"
-                      signaturePath={memberSignatureUrl as string}
-                      citizenshipPath={memberCitizenshipUrl as string}
-                      cardBg="neutralColorLight.Gray-10"
-                    />
-                  )}
-
-                  {memberId && <BulkDepositAccountsTable memberId={memberId} />}
-
-                  {accounts?.length && <BulkDepositAccountsSummary memberId={memberId} />}
-                </Box>
-              </Box>
-
-              <Payment mode={mode} totalDeposit={Number(totalDep || 0) + Number(totalFine || 0)} />
-            </form>
-          </FormProvider>
-        </Box>
-      </Container>
-
-      <Box position="relative" margin="0px auto">
-        <Box bottom="0" position="fixed" width="100%" bg="gray.100" zIndex={10}>
-          <Container minW="container.xl" height="fit-content">
-            <FormFooter
-              status={
-                mode === 0 ? (
-                  <Box display="flex" gap="s32">
-                    <Text fontSize="r1" fontWeight={600} color="neutralColorLight.Gray-50">
-                      Total Payable Amount
-                    </Text>
-                    <Text fontSize="r1" fontWeight={600} color="neutralColorLight.Gray-70">
-                      {Number(totalDep || '0') + Number(totalFine || '0')}
-                    </Text>
-                  </Box>
-                ) : (
-                  <Button variant="solid" onClick={() => setMode(0)}>
-                    Previous
-                  </Button>
-                )
-              }
-              mainButtonLabel={mode === 0 ? 'Proceed Transaction' : 'Submit'}
-              mainButtonHandler={mode === 0 ? () => setMode(1) : handleSubmit}
-              isMainButtonDisabled={disableSubmitButtonFxn(paymentModes) && mode === 1}
-            />
-          </Container>
-        </Box>
-      </Box>
-    </>
+          <Payment mode={mode} totalDeposit={Number(totalDep || 0) + Number(totalFine || 0)} />
+        </FormLayout.Form>
+      </FormLayout.Content>
+      {mode === 0 && (
+        <FormLayout.Footer
+          status={
+            <Box display="flex" gap="s32">
+              <Text fontSize="r1" fontWeight={600} color="neutralColorLight.Gray-50">
+                Total Payable Amount
+              </Text>
+              <Text fontSize="r1" fontWeight={600} color="neutralColorLight.Gray-70">
+                {Number(totalDep || '0') + Number(totalFine || '0')}
+              </Text>
+            </Box>
+          }
+          mainButtonLabel="Proceed Transaction"
+          mainButtonHandler={() => setMode(1)}
+          // isMainButtonDisabled={disableSubmitButtonFxn(paymentModes) && mode === 1}
+        />
+      )}
+      {mode === 1 && (
+        <FormLayout.Footer
+          status={
+            <Button variant="solid" onClick={() => setMode(0)}>
+              Previous
+            </Button>
+          }
+          mainButtonLabel="Submit"
+          mainButtonHandler={handleSubmit}
+          isMainButtonDisabled={disableSubmitButtonFxn(paymentModes) && mode === 1}
+        />
+      )}
+    </FormLayout>
   );
 };
 
