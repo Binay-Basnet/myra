8b5a268b6bcb102c5e5aae1ff21cbb5328df8f7d
Ajay Prajapati <ajayprazz@gmail.com>
2024-02-22 14:12:16 +0545

    feat(cbs): refetch me query on tab focus

--- apps/myra/pages/_app.tsx
+++ apps/myra/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, ReactNode, useEffect } from 'react';
+import { ReactElement, ReactNode, useContext, useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
@@ -12,8 +12,18 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Box, Loader, Toaster } from '@myra-ui';
 import { theme } from '@myra-ui/theme';
 
-import { store, useInit } from '@coop/cbs/data-access';
-import { AbilityContext, buildEmptyPermissions } from '@coop/cbs/utils';
+import {
+  authenticate,
+  logout,
+  RoleInfo,
+  store,
+  useAppDispatch,
+  useGetMeQuery,
+  useInit,
+  useRefreshToken,
+} from '@coop/cbs/data-access';
+import { AbilityContext, buildEmptyPermissions, updateAbility } from '@coop/cbs/utils';
+import { getSchemaPath, useReplace } from '@coop/shared/utils';
 
 import '@raralabs/web-feedback/dist/css/style.css';
 import './app.css';
@@ -42,12 +52,16 @@ const queryClient = new QueryClient({
   },
 });
 
+const url = getSchemaPath();
+
 const MainApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   const { isLoading } = useInit();
 
   const getLayout = Component.getLayout || ((page) => page);
 
   const router = useRouter();
+
+  useTabChange();
 
   const lang = store.getState().auth?.preference?.languageCode;
 
@@ -96,3 +110,86 @@ const CustomApp = (props: AppPropsWithLayout) => (
 );
 
 export default CustomApp;
+
+const useTabChange = () => {
+  const router = useRouter();
+
+  const refreshToken = useRefreshToken(url ?? '');
+
+  const [triggerQuery, setTriggerQuery] = useState(false);
+
+  const dispatch = useAppDispatch();
+  const replace = useReplace();
+
+  const getMe = useGetMeQuery(
+    {},
+    {
+      enabled: triggerQuery,
+    }
+  );
+
+  const hasDataReturned = getMe?.data?.auth;
+  const hasData = getMe?.data?.auth?.me?.data;
+
+  const loginRecord = getMe?.data?.auth?.me;
+  const loginData = loginRecord?.data;
+
+  const userData = loginData?.user;
+  const preference = loginData?.preference;
+  const permissions = loginData?.permission?.myPermission;
+  const availableRoles = loginData?.rolesList;
+  const availableBranches = loginData?.branches;
+
+  const userAbility = useContext(AbilityContext);
+
+  const getRefreshToken = async () => {
+    try {
+      const response = await refreshToken();
+
+      if (response) {
+        queryClient.invalidateQueries(['getMe']);
+        setTriggerQuery(true);
+      }
+    } catch (e) {
+      replace(
+        {
+          pathname: '/login',
+          query: {
+            redirect: router.asPath,
+          },
+        },
+        '/login'
+      ).then(() => {
+        dispatch(logout());
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (hasDataReturned) {
+      if (userData && preference && permissions && availableRoles && availableBranches) {
+        updateAbility(userAbility, permissions as Partial<Record<string, string>>);
+
+        dispatch(
+          authenticate({
+            user: userData,
+            permissions: permissions as Record<string, string>,
+            preference,
+            availableRoles: availableRoles as RoleInfo[],
+            availableBranches: availableBranches as RoleInfo[],
+          })
+        );
+      }
+    }
+  }, [dispatch, hasDataReturned, hasData, userData]);
+
+  if (!router.isReady) {
+    return;
+  }
+
+  if (window) {
+    window.onfocus = () => {
+      getRefreshToken();
+    };
+  }
+};
