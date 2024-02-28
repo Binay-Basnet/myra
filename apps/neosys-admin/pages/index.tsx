c0d73b7063b4401844d9015bf82730b135f78f85
Binay-Basnet <binaybasnetoct13@gmail.com>
2024-02-28 16:33:58 +0545

    test: cache test

--- apps/neosys-admin/pages/index.tsx
+++ apps/neosys-admin/pages/index.tsx
@@ -11,7 +11,7 @@ const Index = () => {
     router.push('/clients');
   }, []);
 
-  return <Box>Dashboard</Box>;
+  return <Box>Dashboard for you jsdagojdsaiogjsadlfjdslfj</Box>;
 };
 
 Index.getLayout = function getLayout(page: ReactElement) {
